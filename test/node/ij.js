f9f8a22790206ad24b24b28d5b9eaec673c59d24
Michaël Zasso <mic.besace@gmail.com>
2015-06-24 10:19:38 +0200

    node: add loadDemo API

--- test/node/ij.js
+++ test/node/ij.js
@@ -4,14 +4,14 @@ var IJ = require('../..');
 
 describe('IJ core', function () {
     it('should load from URL', function () {
-        return IJ.load(__dirname + '/../img/rgb8.png').then(function (img) {
+        return IJ.loadDemo('rgb8.png').then(function (img) {
             img.width.should.be.greaterThan(0);
             img.height.should.be.greaterThan(0);
         });
     });
 
     it('should clone', function () {
-        return IJ.load(__dirname + '/../img/rgb8.png').then(function (img) {
+        return IJ.loadDemo('rgb8.png').then(function (img) {
             var clone = img.clone();
             clone.should.be.an.instanceOf(IJ);
             clone.should.not.be.equal(img);
