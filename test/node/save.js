f42c2cb9a49ef9cf6a02e6db585a3c6251966287
Michaël Zasso <mic.besace@gmail.com>
2015-06-29 15:37:04 +0200

    test that saved image is identical to original

--- test/node/save.js
+++ test/node/save.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var common = require('../common');
+var IJ = common.IJ;
+var getSha = require('ij-test').getHash;
 var load = common.load;
 
 common.refreshTmpDir();
@@ -9,7 +11,13 @@ describe('save to disk', function () {
 
     it('load then save', function () {
         return load('rgb8.png').then(function (img) {
-            return img.save(common.tmpDir + '/img1.png');
+            var sha = getSha(img);
+            return img.save(common.tmpDir + '/img1.png').then(function () {
+                // reload the new file to check that the image is identical
+                return IJ.load(common.tmpDir + '/img1.png').then(function (img) {
+                    getSha(img).should.equal(sha);
+                });
+            });
         });
     });
 
