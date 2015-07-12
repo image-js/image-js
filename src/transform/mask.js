35da3d365ad783f39825b2203e02a08a80393e1e
Luc Patiny <luc@patiny.com>
2015-07-12 20:49:40 +0200

    Add grey algorithm possibilities

--- src/transform/mask.js
+++ src/transform/mask.js
@@ -2,35 +2,39 @@
 
 import IJ from '../ij';
 
-export default function mask({algorithm = '', options = {}} = {}) {
+export default function mask({algorithm = 'threshold', threshold = 127, useAlpha = true} = {}) {
     this.checkProcessable('mask', {
         components: 1,
         bitDepth: [8, 16]
     });
 
     var data = this.data;
-    var threshold=options.threshold || 127;
-
-    var useAlpha=options.useAlpha===false ? false : true;
 
     let newImage = IJ.createFrom(this, {
             kind: 'BINARY'
         }
     );
 
-    if (this.alpha && useAlpha) {
-        for (let i = 0; i < data.length; i += 2) {
-            if ((data[i]*data[i+1]/this.maxValue)>=threshold) {
-                newImage.setBit(i>>1);  // we divide by 2 the geek way ;)
+    switch (algorithm) {
+        case 'threshold':
+            if (this.alpha && useAlpha) {
+                for (let i = 0; i < data.length; i += 2) {
+                    if ((data[i]*data[i+1]/this.maxValue)>=threshold) {
+                        newImage.setBit(i>>1);  // we divide by 2 the geek way ;)
+                    }
+                }
+            } else {
+                for (let i = 0; i < data.length; i++) {
+                    if (data[i]>=threshold) {
+                        newImage.setBit(i);
+                    }
+                }
             }
-        }
-    } else {
-        for (let i = 0; i < data.length; i++) {
-            if (data[i]>=threshold) {
-                newImage.setBit(i);
-            }
-        }
+            break;
+        default:
+            throw new Error('grey transform unknown algorithm: '+algorithm);
     }
+
 
     return newImage;
 }
