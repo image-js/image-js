a235709c8e77e3ede96446077957c33523627718
Luc Patiny <luc@patiny.com>
2015-07-14 19:15:04 +0200

    Change value to 0.5 for threshold instead of 127

--- test/node/filter/mask.js
+++ test/node/filter/mask.js
@@ -8,8 +8,8 @@ describe('Create a mask from a greyA image', function () {
         kind: 'GREYA'
     });
 
-    it('should create a mask for a threshold of 127 using alpha channel', function () {
-        var mask=image.mask(127);
+    it('should create a mask for a threshold of 0.5 using alpha channel', function () {
+        var mask=image.mask(0.5);
 
         mask.channels.should.equal(1);
         mask.bitDepth.should.equal(1);
@@ -23,8 +23,8 @@ describe('Create a mask from a greyA image', function () {
         data[0].should.equal(128);
     });
 
-    it('should create a mask for a threshold of 127 not using alpha channel', function () {
-        var mask=image.mask(127, {useAlpha: false});
+    it('should create a mask for a threshold of 0.5 not using alpha channel', function () {
+        var mask=image.mask(0.5, {useAlpha: false});
 
         mask.channels.should.equal(1);
         mask.bitDepth.should.equal(1);
@@ -39,25 +39,25 @@ describe('Create a mask from a greyA image', function () {
     });
 
 });
-//
-//describe('Create a mask from a greyA image using percentile algorithm', function () {
-//
-//    var image = new IJ(4,1,[0, 255, 63, 255, 127, 255, 255, 255], {
-//        kind: 'GREYA'
-//    });
-//
-//    var mask=image.mask('percentile', {useAlpha: false});
-//
-//    mask.channels.should.equal(1);
-//    mask.bitDepth.should.equal(1);
-//    mask.width.should.equal(4);
-//    mask.height.should.equal(1);
-//
-//    var data=mask.data;
-//    data.should.instanceOf(Uint8Array);
-//    data.length.should.equal(1);
-//
-//    data[0].should.equal(240);
-//
-//
-//});
+
+describe('Create a mask from a greyA image using percentile algorithm', function () {
+
+    var image = new IJ(4,1,[0, 255, 63, 255, 127, 255, 255, 255], {
+        kind: 'GREYA'
+    });
+
+    var mask=image.mask('percentile', {useAlpha: false});
+
+    mask.channels.should.equal(1);
+    mask.bitDepth.should.equal(1);
+    mask.width.should.equal(4);
+    mask.height.should.equal(1);
+
+    var data=mask.data;
+    data.should.instanceOf(Uint8Array);
+    data.length.should.equal(1);
+
+    data[0].should.equal(240);
+
+
+});
