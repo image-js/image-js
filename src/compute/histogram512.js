970e5c667b49aefee700beba456ec2f5d67a9248
Luc Patiny <luc@patiny.com>
2015-07-15 23:45:37 +0200

    Improvement of histogram512 but not yet finished

--- src/compute/histogram512.js
+++ src/compute/histogram512.js
@@ -2,39 +2,31 @@
 
 export function getHistogram512({useAlpha=true} = {}) {
     this.checkProcessable("getHistogram512", {
-        bitDepth: [8],
-        components: 3
+        bitDepth: [8,16],
+        components: [3]
     });
 
-    var bitSlots=Math.log(maxSlots)/Math.log(2);
-    if (bitSlots != Math.floor(bitSlots)) {
-        throw new Error('maxSlots must be a power of 2, for example: '+Math.pow(2, Math.floor(bitSlots)));
-    }
-    // we will compare the bitSlots to the bitDepth of the image
-    // based on this we will shift the values. This allows to generate a histogram
-    // of 16 grey even if the images has 256 shade of grey
-
-    var bitShift=0;
-    if (this.bitDepth>bitSlots) bitShift=this.bitDepth-bitSlots;
+    var bitShift=bitDepth-3;
 
     var data = this.data;
-    var result = new Uint32Array(Math.pow(2, Math.min(this.bitDepth, bitSlots)));
+    var result = new Uint32Array(512);
     if (useAlpha && this.alpha) {
-        let alphaChannelDiff=this.channels-channel-1;
-
-        for (let i = channel; i < data.length; i += this.channels) {
-            if (data[i+alphaChannelDiff]>0) { // we add a point in the histogram only if the value is not completely transparent
-                result[Math.floor((data[i]>>bitShift)*data[i+alphaChannelDiff]/this.maxValue)]++;
+        for (let i = 0; i < data.length; i += this.channels) {
+            if (data[i+this.channels-1]>0) { // we add a point in the histogram only if the value is not completely transparent
+                let ratio=data[i+this.channels-1]/this.maxValue;
+                let slot=((data[i]*ratio)>>bitShift)*64+
+                    ((data[i+1]*ratio)>>bitShift)*8+
+                    ((data[i+2]*ratio)>>bitShift);
+                result[slot]++;
             }
+
         }
     } else {
-        for (let i = channel; i < data.length; i += this.channels) {
-            result[data[i]>>bitShift]++;
+        for (let i = 0; i < data.length; i += this.channels) {
+            let slot=(data[i]>>bitShift)*64+(data[i+1]>>bitShift)*8+(data[i+2]>>bitShift);
+            result[slot]++;
         }
     }
-
-
-
 }
 
 
