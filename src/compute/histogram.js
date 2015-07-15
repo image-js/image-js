e7c6c05b4620a7917fee0dacfdee34bf076ecf60
Luc Patiny <luc@patiny.com>
2015-07-15 19:03:53 +0200

    Allow to specify the number of slots in the histogram compute function

--- src/compute/histogram.js
+++ src/compute/histogram.js
@@ -1,14 +1,19 @@
 'use strict';
 
-export function getHistogram({channel=0, useAlpha=true} = {}) {
+export function getHistogram({maxSlots=256, channel=undefined, useAlpha=true} = {}) {
     this.checkProcessable("getHistogram", {
         bitDepth: [8]
     });
-
-    return getChannelHistogram.call(this, channel, useAlpha);
+    if (channel===undefined) {
+        if (this.components > 1) {
+            throw new Error('You need to define the channel for an image that contains more than one channel');
+        }
+        channel = 0;
+    }
+    return getChannelHistogram.call(this, channel, useAlpha, maxSlots);
 }
 
-export function getHistograms({useAlpha=true} = {}) {
+export function getHistograms({maxSlots=256, useAlpha=true} = {}) {
     this.checkProcessable("getHistograms", {
         bitDepth: [8]
     });
@@ -22,20 +27,31 @@ export function getHistograms({useAlpha=true} = {}) {
 
 
 
-function getChannelHistogram(channel, useAlpha) {
+function getChannelHistogram(channel, useAlpha, maxSlots) {
+    var bitSlots=Math.log(maxSlots)/Math.log(2);
+    if (bitSlots != Math.floor(bitSlots)) {
+        throw new Error('maxSlots must be a power of 2, for example: '+Math.pow(2, Math.floor(bitSlots)));
+    }
+    // we will compare the bitSlots to the bitDepth of the image
+    // based on this we will shift the values. This allows to generate a histogram
+    // of 16 grey even if the images has 256 shade of grey
+
+    var bitShift=0;
+    if (this.bitDepth>bitSlots) bitShift=this.bitDepth-bitSlots;
+
     var data = this.data;
-    var result = new Uint32Array(Math.pow(2, this.bitDepth));
+    var result = new Uint32Array(Math.pow(2, Math.min(this.bitDepth, bitSlots)));
     if (useAlpha && this.alpha) {
         let alphaChannelDiff=this.channels-channel-1;
 
         for (let i = channel; i < data.length; i += this.channels) {
             if (data[i+alphaChannelDiff]>0) { // we add a point in the histogram only if the value is not completely transparent
-                result[Math.floor(data[i]*data[i+alphaChannelDiff]/this.maxValue)]++;
+                result[Math.floor((data[i]>>bitShift)*data[i+alphaChannelDiff]/this.maxValue)]++;
             }
         }
     } else {
         for (let i = channel; i < data.length; i += this.channels) {
-            result[data[i]]++;
+            result[data[i]>>bitShift]++;
         }
     }
 
--- test/node/compute/histogram.js
+++ test/node/compute/histogram.js
@@ -3,18 +3,11 @@
 import {Image} from '../common';
 
 describe('calculate the histogram', function () {
-    it('check histogram property', function () {
+    it('check getHistogram method', function () {
 
         let image = new Image(1,2,[230, 83, 120, 255, 100, 140, 13, 1]);
 
-
-        let histogram=image.histogram;
-        histogram[0].should.equal(1);
-        histogram[230].should.equal(1);
-        histogram[100].should.equal(0);
-        histogram[255].should.equal(0);
-
-        histogram=image.getHistogram({useAlpha: false});
+        let histogram=image.getHistogram({useAlpha: false, channel: 0});
         histogram[0].should.equal(0);
         histogram[100].should.equal(1);
         histogram[230].should.equal(1);
@@ -28,5 +21,35 @@ describe('calculate the histogram', function () {
 
 
     });
+
+
+    it('check histogram property', function () {
+
+        let image = new Image(1,4,[230, 255, 230, 255, 230, 255, 13, 1], {
+            kind:'GREYA'
+        });
+
+        let histogram=image.histogram;
+
+        histogram[0].should.equal(1);
+        histogram[230].should.equal(3);
+        histogram[100].should.equal(0);
+        histogram[255].should.equal(0);
+    });
+
+    it('check 16 slots histogram', function () {
+
+        let image = new Image(1,4,[230, 255, 230, 255, 230, 255, 13, 1], {
+            kind:'GREYA'
+        });
+
+        let histogram=image.getHistogram({maxSlots: 16});
+
+        histogram[0].should.equal(1);
+        histogram[14].should.equal(3);
+        histogram[1].should.equal(0);
+        histogram[15].should.equal(0);
+    });
+
 });
 
