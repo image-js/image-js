fd6962e32cf299cd466046699ae047f8567dfa4f
lpatiny <luc@patiny.com>
2015-07-16 08:49:28 +0200

    Change histogram behaviour with alpha channel. When using the alpha channel the contribution to the histogram is proportional to the alpha. This means that if alpha is 0, this pixels will not be taken into account for the histogram.

--- src/compute/histogram512.js
+++ src/compute/histogram512.js
@@ -9,17 +9,13 @@ export function getHistogram512({useAlpha=true} = {}) {
     var bitShift=bitDepth-3;
 
     var data = this.data;
-    var result = new Uint32Array(512);
+    var result = new Float32Array(512);
     if (useAlpha && this.alpha) {
         for (let i = 0; i < data.length; i += this.channels) {
-            if (data[i+this.channels-1]>0) { // we add a point in the histogram only if the value is not completely transparent
-                let ratio=data[i+this.channels-1]/this.maxValue;
-                let slot=((data[i]*ratio)>>bitShift)*64+
-                    ((data[i+1]*ratio)>>bitShift)*8+
-                    ((data[i+2]*ratio)>>bitShift);
-                result[slot]++;
-            }
-
+                let slot=((data[i])>>bitShift)*64+
+                    ((data[i+1])>>bitShift)*8+
+                    ((data[i+2])>>bitShift);
+                result[slot]+=data[i+this.channels-1]/this.maxValue;
         }
     } else {
         for (let i = 0; i < data.length; i += this.channels) {
