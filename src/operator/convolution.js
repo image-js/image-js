ec2341ae1955f406a60a55224142782fea6af94b
Cristian Ballesteros <cristian.ballesteros@correounivalle.edu.co>
2015-07-17 23:47:30 -0500

    Edge Handling Methods were in another file and 'mirror' method was implemented

--- src/operator/convolution.js
+++ src/operator/convolution.js
@@ -1,22 +1,28 @@
 'use strict'
 
+import edgeHandlingMethod from './edgeHandlingMethod'
+
 //convolution using a square kernel
 export default function convolution(newImage, kernel, edgeHandling){
     let kernelWidth = Math.sqrt(kernel.length);
     let k = Math.floor(kernelWidth/2);
     let div = 0;
-    let sum;
+    edgeHandling = edgeHandling || 'copy';
 
     for(let i = 0; i < kernel.length; i++){
         div += kernel[i];
     }
 
-    for(let x = k; x < this.width - k; x++){
-        for(let y = k; y < this.height - k; y++){
-            sum = 0;
+    for(let x = 0; x < this.width ; x++){
+        for(let y = 0; y < this.height ; y++){
+            let sum = 0;
             for(let i = -k; i <= k; i++){
                 for(let j = -k; j <= k; j++){
-                    sum += this.getValueXY(x + i, y + j, 0)*kernel[(i + k)*kernelWidth + (j + k)];
+                    let val = edgeHandling.toLowerCase() == 'mirror'
+                        ? mirrorValue(x, y, i, j, this)
+                        : (isOutSidePixel(x, y, this) ? undefined : this.getValueXY(x + i, y + j, 0));
+                    if(val != undefined)
+                        sum += val*kernel[(i + k)*kernelWidth + (j + k)];
                 }
             }
             let newValue = Math.floor(sum/div);
@@ -27,51 +33,26 @@ export default function convolution(newImage, kernel, edgeHandling){
         }
     }
 
-    console.log(edgeHandling);
+    if(edgeHandling.toLowerCase() != 'mirror'){
+        edgeHandlingMethod.call(this, newImage, edgeHandling.toLowerCase(), k);
+    }
 
-    if(edgeHandling != undefined)
-        if(edgeHandling.toLowerCase() == 'black'){
-            for(let x = 0; x < this.width; x++){
-                for(let y = 0; y <= k; y++){
-                    newImage.setValueXY(x, y, 0, 0);
-                    newImage.setValueXY(x, y, 1, this.getValueXY(x, y, 1));
-                }
-                for(let y = this.height-k; y < this.height; y++){
-                    newImage.setValueXY(x, y, 0, 0);
-                    newImage.setValueXY(x, y, 1, this.getValueXY(x, y, 1));
-                }
-            }
-            for(let x = 0; x < this.height; x++){
-                for(let y = 0; y <= k; y++){
-                    newImage.setValueXY(y, x, 0, 0);
-                    newImage.setValueXY(y, x, 1, this.getValueXY(y, x, 1));
-                }
-                for(let y = this.width-k; y < this.width; y++){
-                    newImage.setValueXY(y, x, 0, 0);
-                    newImage.setValueXY(y, x, 1, this.getValueXY(y, x, 1));
-                }
-            }
-        }else if(edgeHandling.toLowerCase() == 'copy'){
-            for(let x = 0; x < this.width; x++){
-                for(let y = 0; y <= k; y++){
-                    newImage.setValueXY(x, y, 0, this.getValueXY(x, y, 0));
-                    newImage.setValueXY(x, y, 1, this.getValueXY(x, y, 1));
-                }
-                for(let y = this.height-k; y < this.height; y++){
-                    newImage.setValueXY(x, y, 0, this.getValueXY(x, y, 0));
-                    newImage.setValueXY(x, y, 1, this.getValueXY(x, y, 1));
-                }
-            }
-            for(let x = 0; x < this.height; x++){
-                for(let y = 0; y <= k; y++){
-                    newImage.setValueXY(y, x, 0, this.getValueXY(y, x, 0));
-                    newImage.setValueXY(y, x, 1, this.getValueXY(y, x, 1));
-                }
-                for(let y = this.width-k; y < this.width; y++){
-                    newImage.setValueXY(y, x, 0, this.getValueXY(y, x, 0));
-                    newImage.setValueXY(y, x, 1, this.getValueXY(y, x, 1));
-                }
-            }
-        }
+}
 
+function isOutSidePixel(x,y,im){
+    return x > im.width || x < 0 || y > im.height || y < 0;
+}
+
+function mirrorValue(x,y,i,j,im){
+    if(!isOutSidePixel(x+i,y+j,im)){
+        return im.getValueXY(x+i,y+j,0);
+    }else if(!isOutSidePixel(x-i,y+j,im)){
+        return im.getValueXY(x-i,y+j,0);
+    }else if(!isOutSidePixel(x+i,y-j,im)){
+        return im.getValueXY(x+i,y-j,0);
+    }else if(!isOutSidePixel(x-i,y-j,im)){
+        return im.getValueXY(x-i,y-j,0);
+    }else{
+        return 0;
+    }
 }
