fc79116a4b5e16067c79995d1e3a0c2bb0d8053d
Luc Patiny <luc@patiny.com>
2015-09-19 09:55:17 +0200

    Check level filter with all images format

--- test/node/filter/level.js
+++ test/node/filter/level.js
@@ -1,4 +1,4 @@
-import {Image} from '../common';
+import {Image, load, getHash} from '../common';
 
 describe('level', function() {
     describe('extend the image to cover all levels', function () {
@@ -24,6 +24,44 @@ describe('level', function() {
 
         });
 
+        it('should not change at all an image that has the extreme colors', function() {
+            return load('rgb24bits.png').then(
+                function(image) {
+                    let hash=getHash(image);
+                    image.level();
+                    let hash2=getHash(image);
+                    hash.should.equal(hash2);
+                }
+            );
+        });
+
+        it('should change for a normal picture', function() {
+            return load('cat.jpg').then(
+                function(image) {
+                    let hash=getHash(image);
+                    image.level();
+                    let hash2=getHash(image);
+                    hash.should.not.equal(hash2);
+                }
+            );
+        })
+
+
     });
+
+    describe('testing various file format', function() {
+        const tests = ['grey8','greya16','greya32','rgb24','rgb48','rgba32','rgba64'];
+        tests.forEach(function(test) {
+            it('should not change: format/'+test, function() {
+                return load('format/' + test + '.png').then(function(image) {
+                    let hash=getHash(image);
+                    image.level();
+                    let hash2=getHash(image);
+                    hash.should.equal(hash2);
+                });
+            });
+        });
+    });
+
 });
 
