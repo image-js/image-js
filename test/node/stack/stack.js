94fb874b1c866ae11057e5267a8bda0a01df34f2
Michaël Zasso <mic.besace@gmail.com>
2015-09-25 11:13:56 +0200

    add test for Stack prototype

--- test/node/stack/stack.js
+++ test/node/stack/stack.js
@@ -4,11 +4,27 @@ describe('Core methods of Stack objects', function () {
 
     let stack = new Stack([getSquare(), getSquare()]);
 
-    it('map', function () {
+    it('should be an Array', function () {
+        stack.should.be.instanceOf(Stack);
+        stack.should.be.instanceOf(Array);
+        Array.isArray(stack).should.be.true();
+    });
+
+    it('should have Array methods on prototype', function () {
+        stack.forEach(function (image) {
+            image.should.be.instanceOf(Image);
+        });
+        stack.filter(function (image) {
+            return false;
+        }).should.have.lengthOf(0);
+    });
+
+    it('map should return a Stack', function () {
         let result = stack.map(function (image) {
             return image.grey();
         });
 
+        result.should.be.instanceOf(Stack);
         result.length.should.equal(2);
         result[0].components.should.equal(1);
 
