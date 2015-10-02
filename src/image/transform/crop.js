cc382bb8ee13e3aa9b8d9ef4f3037490a97541ec
Luc Patiny <luc@patiny.com>
2015-10-02 14:40:55 +0200

    Fix error message syntax in crop (back tick)

--- src/image/transform/crop.js
+++ src/image/transform/crop.js
@@ -10,11 +10,11 @@ export default function crop({
     if (x > (this.width - 1) || y > (this.height - 1))
         throw new RangeError(`crop: origin (x:${x}, y:${y}) out of range (${this.width - 1}; ${this.height - 1})`);
     if (width <= 0 || height <= 0)
-        throw new RangeError('crop: width and height (width:${width}; height:${height}) must be positive numbers');
+        throw new RangeError(`crop: width and height (width:${width}; height:${height}) must be positive numbers`);
     if (x < 0 || y < 0)
-        throw new RangeError('crop: x and y (x:${x}, y:${y}) must be positive numbers');
+        throw new RangeError(`crop: x and y (x:${x}, y:${y}) must be positive numbers`);
     if (width > (this.width - x) || height > (this.height - y))
-        throw new RangeError('crop: (x: ${x}, y:${y}, width:${width}, height:${height}) size is out of range');
+        throw new RangeError(`crop: (x: ${x}, y:${y}, width:${width}, height:${height}) size is out of range`);
 
     let newImage = Image.createFrom(this, {width, height});
 
