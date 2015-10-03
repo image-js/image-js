566d4fc716524321e4a1b15b69b43252dd61247b
Luc Patiny <luc@patiny.com>
2015-10-03 11:50:14 +0200

    Add more test cases for mask

--- src/image/transform/mask/mask.js
+++ src/image/transform/mask/mask.js
@@ -1,38 +1,36 @@
 import Image from '../../image';
 
 import percentile from './percentile';
-
+import {getThreshold} from '../../../util/converter';
 
 /*
  Creation of binary mask is based on the determination of a threshold
  You may either choose among the provided algorithm or just specify a threshold value
- If the algorithm is a number, it is the threshold value
  */
 
 
 export default function mask({
-    algorithm = 0.5,
+    algorithm = 'threshold',
+    threshold = 0.5,
     useAlpha = true,
     invert = false
     } = {}) {
+
     this.checkProcessable('mask', {
         components: 1,
         bitDepth: [8,16]
     });
 
-    let threshold = 0;
-
-    if (typeof algorithm === 'number') {
-        threshold = algorithm * this.maxValue;
-    } else {
-        let histogram = this.getHistogram();
-        switch (algorithm.toLowerCase()) {
-            case 'percentile':
-                threshold = percentile(histogram);
-                break;
-            default:
-                throw new Error('mask transform unknown algorithm: ' + algorithm);
-        }
+    let histogram = this.getHistogram();
+    switch (algorithm.toLowerCase()) {
+        case 'threshold':
+            threshold = getThreshold(threshold, this.maxValue);
+            break;
+        case 'percentile':
+            threshold = percentile(histogram);
+            break;
+        default:
+            throw new Error('mask transform unknown algorithm: ' + algorithm);
     }
 
     let newImage = new Image (this.width, this.height, {
--- test/node/transform/mask/mask.js
+++ test/node/transform/mask/mask.js
@@ -1,4 +1,26 @@
-import {Image} from '../common';
+import {Image} from '../../common';
+
+describe('Create mask from a GREY image', function () {
+    it('From GREY image give a mask image', function () {
+        let image = new Image(5, 1,
+            [
+                0, 63, 127, 191, 255
+            ],
+            {kind: 'GREY'}
+        );
+
+        image.mask().data[0].should.equal(0b00011000);
+        image.mask({threshold: 0.9}).data[0].should.equal(0b00001000);
+        image.mask({threshold: 128}).data[0].should.equal(0b00011000);
+        image.mask({threshold: '50%'}).data[0].should.equal(0b00011000);
+        image.mask({threshold: '50%', invert: true}).data[0].should.equal(0b11100000);
+        image.mask({algorithm: 'percentile'}).data[0].should.equal(0b11111000);
+        (function () {
+            image.mask({algorithm: 'XXX'}).should.throw(/Unsupported grey algorithm/);
+        });
+
+    });
+});
 
 describe('Create a mask from a greyA image', function () {
 
@@ -8,7 +30,7 @@ describe('Create a mask from a greyA image', function () {
 
 
     it('should create a mask for a threshold of 0.5 using alpha channel', function () {
-        let mask = image.mask({algorithm: 0.5});
+        let mask = image.mask({threshold: 0.5});
 
         mask.channels.should.equal(1);
         mask.bitDepth.should.equal(1);
