7529bd8ae16e4fa1d42528d0c37452ea833378f2
Luc Patiny <luc@patiny.com>
2015-10-03 11:50:49 +0200

    Add getThreshold utility to deal with 100%, 0.5 and 128

--- src/util/converter.js
+++ src/util/converter.js
@@ -13,6 +13,35 @@ export function getFactor(value) {
     return value;
 }
 
+/**
+ * We can specify a threshold as "0.4", "40%" or 123
+ * @param value
+ * @param maxValue
+ * @returns {*}
+ */
+export function getThreshold(value, maxValue) {
+    if (!maxValue) {
+        throw Error('getThreshold : the maxValue should be specified');
+    }
+    if (typeof value === 'string') {
+        let last = value[value.length - 1];
+        if (last !== '%') {
+            throw Error('getThreshold : if the value is a string it must finish by %');
+        }
+        return parseFloat(value) / 100 * maxValue;
+    } else if (typeof value === 'number') {
+        if (value < 1) {
+            return value * maxValue;
+        }
+        return value;
+    } else {
+        throw Error('getThreshold : the value is not valid');
+    }
+    return value;
+}
+
+
+
 export function factorDimensions(factor, width, height) {
     factor = getFactor(factor);
     return {
