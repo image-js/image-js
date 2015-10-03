8ed775d3a9ba7f83ba9bd80b27b275a961d3b2bd
Luc Patiny <luc@patiny.com>
2015-10-03 15:46:29 +0200

    Add kernel utilities

--- src/image/operator/convolution.js
+++ src/image/operator/convolution.js
@@ -1,7 +1,7 @@
-import isInteger from 'is-integer';
+
 import Image from '../image';
 import validateArrayOfChannels from '../utility/validateArrayOfChannels';
-
+import {validateKernel} from '../../util/kernel';
 /**
  *
  * @param kernel
@@ -15,40 +15,10 @@ export default function convolution(kernel, {channels, bitDepth, normalize = fal
 
     let newImage = Image.createFrom(this, {bitDepth: bitDepth});
 
-    let kWidth, kHeight;
-
-
     channels = validateArrayOfChannels(this, channels, true);
 
-    if (Array.isArray(kernel)) {
-        if (Array.isArray(kernel[0])) { // 2D array
-            if (((kernel.length & 1) === 0) || ((kernel[0].length & 1) === 0))
-                throw new RangeError('convolution: Kernel rows and columns should be odd numbers');
-            else {
-                kHeight = Math.floor(kernel.length / 2);
-                kWidth = Math.floor(kernel[0].length / 2);
-            }
-        } else {
-            let kernelWidth = Math.sqrt(kernel.length);
-            if (isInteger(kernelWidth)) {
-                kWidth = kHeight = Math.floor(Math.sqrt(kernel.length) / 2);
-            } else {
-                throw new RangeError('convolution: Kernel array should be a square');
-            }
-            // we convert the array to a matrix
-            let newKernel = new Array(kWidth);
-            for (let i = 0; i < kernelWidth; i++) {
-                newKernel[i] = new Array(kernelWidth);
-                for (let j = 0; j < kernelWidth; j++) {
-                    newKernel[i][j] = kernel[i * kernelWidth + j];
-                }
-            }
-            kernel = newKernel;
-
-        }
-    } else {
-        throw new Error('convolution: Invalid Kernel: ' + kernel);
-    }
+    let kWidth, kHeight;
+    ({kWidth, kHeight, kernel} = validateKernel(kernel));
 
     //calculate divisor
     if (normalize) {
