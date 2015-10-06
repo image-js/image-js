8988f5ef4c365cb2e941233069eb08f44af0924f
Cristian Ballesteros <cristian.ballesteros@correounivalle.edu.co>
2015-10-05 19:46:06 -0500

    First release for Triangle thresholding method

--- src/image/transform/mask/mask.js
+++ src/image/transform/mask/mask.js
@@ -1,6 +1,7 @@
 import Image from '../../image';
 
 import percentile from './percentile';
+import triangle from './triangle';
 import {getThreshold} from '../../../util/converter';
 
 /*
@@ -28,6 +29,9 @@ export default function mask({
             break;
         case 'percentile':
             threshold = percentile(histogram);
+            break;
+        case 'triangle':
+            threshold = triangle(histogram);
             break;
         default:
             throw new Error('mask transform unknown algorithm: ' + algorithm);
