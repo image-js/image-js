f1b24ee26c7fa2e1cc2c86426047769ecfb540fb
Cristian Ballesteros <cristian.ballesteros@correounivalle.edu.co>
2015-10-05 19:08:58 -0500

    First release for Shanbhag Autothreshold Method

--- src/image/transform/mask/mask.js
+++ src/image/transform/mask/mask.js
@@ -1,6 +1,7 @@
 import Image from '../../image';
 
 import percentile from './percentile';
+import shanbhag from  './shanbhag';
 import {getThreshold} from '../../../util/converter';
 
 /*
@@ -28,6 +29,9 @@ export default function mask({
             break;
         case 'percentile':
             threshold = percentile(histogram);
+            break;
+        case 'shanbhag':
+            threshold = shanbhag(histogram, this.size);
             break;
         default:
             throw new Error('mask transform unknown algorithm: ' + algorithm);
