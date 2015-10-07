59f2b649eeab32071c81647ff63d8747d339db9b
Michaël Zasso <mic.besace@gmail.com>
2015-10-07 15:22:09 +0200

    remove .only

--- test/node/transform/mask/mean.js
+++ test/node/transform/mask/mean.js
@@ -1,7 +1,7 @@
 import {load} from '../../common';
 import mean from '../../../../src/image/transform/mask/mean';
 
-describe.only('Mean threshold', function () {
+describe('Mean threshold', function () {
     it('Should work like ImageJ', function () {
         return load('grayscale_by_zimmyrose.png').then(function (img) {
             mean(img.histogram, img.size).should.equal(106);
