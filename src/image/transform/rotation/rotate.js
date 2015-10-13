74278ec920b88151db888f8e5f96298f737967cc
Michaël Zasso <mic.besace@gmail.com>
2015-10-13 16:05:50 +0200

    precompute fixed values (5x performance gain)

--- src/image/transform/rotation/rotate.js
+++ src/image/transform/rotation/rotate.js
@@ -9,16 +9,20 @@ export default function rotate(degrees, {
         throw new RangeError('width is out of range ' + ((width < 0) ? ' < 0' : (' > ' + this.width)));
     if (height > this.height || height < 0)
         throw new RangeError('height is out of range ' + (height < 0) ? ' < 0' : (' > ' + this.height));
-    let radians = (degrees * Math.PI) / 180;
-    let newImage = Image.createFrom(this, {width , height});
+    const radians = (degrees * Math.PI) / 180;
+    const newImage = Image.createFrom(this, {width, height});
+    const cos = Math.cos(-radians);
+    const sin = Math.sin(-radians);
+    const halfW = Math.round(width / 2);
+    const halfH = Math.round(height / 2);
     for (let i = 0; i < width; i += 1) {
-        for (let j = 0 ; j < height; j += 1) {
-            for(let c = 0; c<this.channels; c++) {
-                let x = Math.round((i - (width / 2)) * Math.cos( - radians) - (j - (height / 2)) * Math.sin(-radians)) + Math.round(width / 2);
-                let y = Math.round((j - (height / 2)) * Math.cos( - radians) + (i - (width / 2)) * Math.sin(-radians)) + Math.round(height / 2);
-                if(x > 0 && x < width) {
-                    if(y > 0 && y < height ) {
-                        newImage.setValueXY(i,j,c,this.getValueXY(x,y,c));
+        for (let j = 0; j < height; j += 1) {
+            for (let c = 0; c < this.channels; c++) {
+                let x = Math.round((i - (width / 2)) * cos - (j - (height / 2)) * sin) + halfW;
+                let y = Math.round((j - (height / 2)) * cos + (i - (width / 2)) * sin) + halfH;
+                if (x > 0 && x < width) {
+                    if (y > 0 && y < height) {
+                        newImage.setValueXY(i, j, c, this.getValueXY(x, y, c));
                     }
                 }
             }
