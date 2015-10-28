3a1a9a5dfabb75791871f35162963371b1175330
madeleine01 <lookingglass0188>
2015-10-28 14:17:58 -0500

    Access to image data were corrected

--- src/image/transform/scale/bilinear.js
+++ src/image/transform/scale/bilinear.js
@@ -1,6 +1,7 @@
 /*
  ***************************************************************************
- *  bilinear interpolation
+ * Method bilinear interpolation
+ * see http://tech-algorithm.com/articles/bilinear-image-scaling/
  ***************************************************************************
  */
 
@@ -16,24 +17,28 @@ export default function bilinear(newImage, newWidth, newHeight) {
     let offset = 0;
     for (let i = 0; i < newHeight; i++) {
         for (let j = 0; j < newWidth; j++) {
-            for (let c = 0; c < this.components; c++) {
+
                 let x0 = (x_ratio * j);
                 let y0 = (y_ratio * i);
                 let x = x0 | 0;
                 let y = y0 | 0;
                 let x_diff = x0 - x;
                 let y_diff = y0 - y;
-                let index = (y * this.width + x) * this.components;
 
-                let A = this.getValue(index, c);
-                let B = this.getValue(index + 1, c);
-                let C = this.getValue(index + this.width, c);
-                let D = this.getValue(index + this.width + 1, c);
+            for (let c = 0; c < this.channels; c++) {
+
+                let index = (y * this.width + x) * this.channels + c;
+
+                let A = this.data[index];
+                let B = this.data[index + this.channels];
+                let C = this.data[index + this.width * this.channels ];
+                let D = this.data[index + this.width * this.channels + this.channels];
 
                 let result = (A * (1 - x_diff) * (1 - y_diff) + B * (x_diff) * (1 - y_diff) + C * (y_diff) * (1 - x_diff)
                     + D * (x_diff * y_diff)) | 0;
 
-                newImage.setValue(offset++, c, result);
+                newImage.data[offset++] = result;
+
             }
         }
     }
