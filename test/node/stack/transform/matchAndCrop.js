8d49decd3fe77ace578aeebb456bcd00950000e8
Michaël Zasso <mic.besace@gmail.com>
2015-11-04 08:26:01 +0100

    remove log

--- test/node/stack/transform/matchAndCrop.js
+++ test/node/stack/transform/matchAndCrop.js
@@ -263,9 +263,6 @@ describe('check matchAndCrop method', function () {
         let results = images.matchAndCrop({algorithm:'matchToFirst'});
         results.should.be.instanceOf(Stack).and.have.lengthOf(3);
 
-        console.log(results[1].data);
-        console.log(results[2].data);
-
         getHash(results[1]).should.equal(getHash(results[2]));
 
         let result = results[0];
