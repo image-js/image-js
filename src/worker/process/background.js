4f03bd411d984b360bb8c17c270e8511799c966e
Michaël Zasso <mic.besace@gmail.com>
2015-11-23 10:53:31 +0100

    handle options

--- src/worker/process/background.js
+++ src/worker/process/background.js
@@ -1,8 +1,29 @@
+import extend from 'extend';
+
+const defaultOptions = {
+    regression: {
+        kernelType: 'polynomial',
+        kernelOptions: {degree: 2, constant: 1}
+    },
+    threshold: 0.02,
+    roi: {
+        minSurface: 100,
+        positive: false
+    },
+    sampling: 20,
+    include: []
+};
+
 function run(image, options) {
+    options = extend({}, defaultOptions, options);
     const manager = this.manager;
     if (Array.isArray(image)) {
         return Promise.all(image.map(function (img) {
-            return runOnce(manager, img, options);
+            const run = runOnce(manager, img, options);
+            if (typeof options.onStep === 'function') {
+                run.then(options.onStep);
+            }
+            return run;
         }));
     } else {
         return runOnce(manager, image, options);
@@ -18,25 +39,43 @@ function runOnce(manager, image, options) {
 function work() {
     worker.on('data', function (send, image, options) {
         image = new IJS(image);
-        const grey = image.grey();
-        const bg = localizeBackground(grey, options);
-        const background = image.getBackground(bg.xyS, bg.zS, options.backgroundOptions);
-        const corrected = image.subtract(background);
-        send(corrected, corrected.data.buffer);
-    });
+        const result = {};
+        const toTransfer = [];
 
-    function localizeBackground(image, options) {
-        const sobel = image.sobelFilter();
-        const mask = sobel.level().mask({threshold: options.backgroundThreshold});
+        const grey = image.grey();
+
+        const sobel = grey.sobelFilter();
+        maybeInclude('sobel', sobel);
+
+        const mask = sobel.level().mask({threshold: options.threshold});
+        maybeInclude('mask', mask);
+
         const roiManager = sobel.getROIManager();
         roiManager.putMask(mask);
-        const realMask = roiManager.getMask(options.backgroundRoiOptions);
-        const pixels = image.getPixelsGrid({
-            sampling: options.backgroundSampling,
+        const realMask = roiManager.getMask(options.roi);
+        maybeInclude('realMask', realMask);
+
+        const pixels = grey.getPixelsGrid({
+            sampling: options.sampling,
             mask: realMask
         });
-        return pixels;
-    }
+
+        const background = image.getBackground(pixels.xyS, pixels.zS, options.regression);
+        maybeInclude('background', background);
+
+        const corrected = image.subtract(background);
+
+        result.result = corrected;
+        toTransfer.push(corrected.data.buffer);
+        send(result, toTransfer);
+
+        function maybeInclude(name, image) {
+            if (options.include.indexOf(name) !== -1) {
+                result[name] = image;
+                toTransfer.push(image.data.buffer);
+            }
+        }
+    });
 }
 
 export default {run, work};
