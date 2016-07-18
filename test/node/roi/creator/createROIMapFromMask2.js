6c675bdd24d83cce4484b7ca5825d53024cb8500
Michaël Zasso <targos@protonmail.com>
2016-07-18 10:44:30 +0200

    test: update roiMap2 test to show that ROI IDs are not sequential

--- test/node/roi/creator/createROIMapFromMask2.js
+++ test/node/roi/creator/createROIMapFromMask2.js
@@ -6,8 +6,8 @@ describe('we check createROIMapFromMask2', function () {
     /*
      We will create the following mask
       ______
-     | x   x|
-     |  x  x|
+     | x x x|
+     |  xx x|
      | x  x |
      |     x|
      | xxx  |
@@ -17,6 +17,8 @@ describe('we check createROIMapFromMask2', function () {
      */
     let mask = new Image(6, 6, {kind: 'BINARY'});
     mask.setBitXY(1, 0);
+    mask.setBitXY(3, 0);
+    mask.setBitXY(3, 1);
     mask.setBitXY(5, 0);
     mask.setBitXY(2, 1);
     mask.setBitXY(5, 1);
@@ -30,28 +32,30 @@ describe('we check createROIMapFromMask2', function () {
     it('should yield the right map with 4 neighbours', function () {
         let mapPixels = ROIMapper(mask, {neighbours: 4}).pixels;
 
-        Array.from(mapPixels).should.eql([
-            0, 1, 0, 0, 0, 2,
-            0, 0, 3, 0, 0, 2,
-            0, 4, 0, 0, 5, 0,
-            0, 0, 0, 0, 0, 6,
-            0, 7, 7, 7, 0, 0,
+        const expected = [
+            0, 1, 0, 2, 0, 3,
+            0, 0, 2, 2, 0, 3,
+            0, 5, 0, 0, 6, 0,
+            0, 0, 0, 0, 0, 7,
+            0, 8, 8, 8, 0, 0,
             0, 0, 0, 0, 0, 0
-        ]);
+        ];
+
+        Array.from(mapPixels).should.eql(expected);
     });
 
     it('should yield the right map with 8 neighbours', function () {
         let mapPixels = ROIMapper(mask, {neighbours: 8}).pixels;
 
-        Array.from(mapPixels).should.eql([
-            0, 1, 0, 0, 0, 2,
-            0, 0, 1, 0, 0, 2,
-            0, 1, 0, 0, 2, 0,
-            0, 0, 0, 0, 0, 2,
-            0, 3, 3, 3, 0, 0,
+        const expected = [
+            0, 1, 0, 1, 0, 1,
+            0, 0, 1, 1, 0, 1,
+            0, 1, 0, 0, 1, 0,
+            0, 0, 0, 0, 0, 1,
+            0, 4, 4, 4, 0, 0,
             0, 0, 0, 0, 0, 0
-        ]);
+        ];
+
+        Array.from(mapPixels).should.eql(expected);
     });
 });
-
-
