9c4d6dce1dc1e68ea7610907d764a49fbefee85e
Michaël Zasso <targos@protonmail.com>
2016-07-18 16:23:23 +0200

    test on bigger image

--- test/nodeScript/roi/roiMap2.js
+++ test/nodeScript/roi/roiMap2.js
@@ -2,19 +2,17 @@
 
 var Image = require('../../..');
 
-Image.load('./test/img/BW15x15.png').then(function (img) {
+Image.load('./test/img/moon/nocrop/BloodMoonTest-6.png').then(function (img) {
+    img = img.scale({factor:4});
     var roiManager = img.getROIManager();
     var mask = img.grey().mask();
 
     console.time('test');
-    for(var i = 0; i < 10000; i++) {
+    //for(var i = 0; i < 10000; i++) {
         roiManager.putMask2(mask);
-    }
+    //}
     console.timeEnd('test');
 
 }, function (err) {
     console.error(err);
 });
-
-
-
