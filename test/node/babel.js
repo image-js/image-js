1a2b6b29a5e924a7120e313b0f237debb04f3946
Michaël Zasso <targos@protonmail.com>
2016-07-25 10:14:57 +0200

    allow running tests from webstorm

--- test/node/babel.js
+++ test/node/babel.js
@@ -1,7 +1,4 @@
 /* eslint-disable strict */
 'use strict';
 
-require('babel-register')({
-    presets: ['es2015-node4'],
-    plugins: ['transform-es2015-block-scoping']
-});
+require('babel-register');
