cecd13693ff6753e010dd441f5a93c854d620ca1
Julien Dénéréaz <denereaz.julien@gmail.com>
2016-08-05 14:22:32 +0200

    expose static kernels and add laplacianOfGaussian

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import {env} from './image/environment';
 
 module.exports = exports = require('./image/image').default;
 exports.Stack = require('./stack/stack').default;
+exports.Kernel = require('./kernel/kernel');
 
 if (env === 'browser') {
     exports.Worker = require('./worker/worker').default;
