48c5e055a6f732080b73ce64ffa1f268fa9e146c
Luc Patiny <luc@patiny.com>
2016-08-09 17:23:38 +0200

    Update local extrema method to return array of x,y,z objects

--- src/image/compute/localExtrema.js
+++ src/image/compute/localExtrema.js
@@ -4,20 +4,25 @@
  * @memberof Image
  * @param mask region of the image that is analyzed. The rest is omitted.
  * @param region 1, 2 or 3. Define the region around each points that is analyzed.
- * @param removeClosePts Remove pts which have a distance between them smaller than this param.
+ * @param removeClosePoints Remove pts which have a distance between them smaller than this param.
  * @param algorithm chose between min or max local.
  * @returns {[number]} Array having has size the number of channels
  */
 
 
-export default function localExtrema(
+export default function localExtrema (
     {
-        removeClosePts = 0,
+        removeClosePoints = 0,
         region = 3,
         algorithm = 'max',
         mask
     } = {}
 ) {
+    let searchMaxima=true;
+    if (algorithm.toLowerCase() === 'min') {
+        searchMaxima=false;
+    }
+
     let image = this;
     this.checkProcessable('localExtrema', {
         bitDepth: [8, 16],
@@ -27,48 +32,43 @@ export default function localExtrema(
 
     let dx = [+1, 0, -1, 0, +1, +1, -1, -1, +2, 0, -2, 0, +2, +2, -2, -2];
     let dy = [0, +1, 0, -1, +1, -1, +1, -1, 0, +2, 0, -2, +2, -2, +2, -2];
-    let pts = [];
+    let points = [];
     for (let currentY = 2; currentY < image.height - 2; currentY++) {
         for (let currentX = 2; currentX < image.width - 2; currentX++) {
             if (mask && !mask.getBitXY(currentX, currentY)) {
                 continue;
             }
-            let min = 0;
+            let counter = 0;
             let currentValue = image.data[currentX + currentY * image.width];
             for (let dir = 0; dir < region; dir++) {
-                if (algorithm === 'min') {
-                    if (image.data[currentX + dx[dir] + (currentY + dy[dir]) * image.width] > currentValue) {
-                        min++;
-                    }
-
-                } else if (algorithm === 'max') {
+                if (searchMaxima) {
                     if (image.data[currentX + dx[dir] + (currentY + dy[dir]) * image.width] < currentValue) {
-                        min++;
+                        counter++;
+                    }
+                } else {
+                    if (image.data[currentX + dx[dir] + (currentY + dy[dir]) * image.width] > currentValue) {
+                        counter++;
                     }
                 }
             }
-            if (min >= region) {
-                pts.push({x: currentX, y: currentY});
+            if (counter === region) {
+                points.push({x: currentX, y: currentY, z: image.data[currentX + currentY * image.width]});
             }
-
         }
     }
-    if (removeClosePts > 0) {
-        for (let i = 0; i < pts.length; i++) {
-            for (let j = i + 1; j < pts.length; j++) {
-                if (Math.sqrt(Math.pow(pts[i].x - pts[j].x, 2) + Math.pow(pts[i].y - pts[j].y, 2)) < removeClosePts) {
-                    pts.splice(j, 1);
+    if (removeClosePoints > 0) {
+        for (let i = 0; i < points.length; i++) {
+            for (let j = i + 1; j < points.length; j++) {
+                if (Math.sqrt(Math.pow(points[i].x - points[j].x, 2) + Math.pow(points[i].y - points[j].y, 2)) < removeClosePoints) {
+                    points[i].x=(points[i].x+points[j].x)>>1;
+                    points[i].y=(points[i].y+points[j].y)>>1;
+                    points[i].z=(points[i].z+points[j].z)>>1;
+                    points.splice(j, 1);
                     j--;
                 }
             }
         }
     }
-    let uniqueId = 1;
-    for (let i = 0; i < pts.length; i++) {
-        pts[i].id = uniqueId;
-        uniqueId++;
-    }
-
-    return pts;
+    return points;
 }
 
--- test/node/compute/localExtrema.js
+++ test/node/compute/localExtrema.js
@@ -1,7 +1,7 @@
 import {Image} from '../common';
 
 
-describe('fin local', function () {
+describe.only('Find local extrema', function () {
     it('maximum for a GREY image', function () {
 
         let image = new Image(10,10,
@@ -20,7 +20,7 @@ describe('fin local', function () {
             ],
             {kind: 'GREY'}
         );
-        image.getLocalExtrema().should.eql([{id:1, x:3, y:2}, {id:2, x:6, y:7}]);
+        image.getLocalExtrema().should.eql([{x:3, y:2, z:4}, {x:6, y:7, z:4}]);
     });
     it('minimum for a GREY image', function () {
 
@@ -41,6 +41,26 @@ describe('fin local', function () {
             {kind: 'GREY'}
         );
 
-        image.getLocalExtrema({algorithm:'min'}).should.eql([{id:1, x:3, y:2}, {id:2, x:6, y:7}]);
+        image.getLocalExtrema({algorithm:'min'}).should.eql([{x:3, y:2, z:2}, {x:6, y:7, z:2}]);
+    });
+    it('maximum for a GREY image with merge', function () {
+        let image = new Image(10,10,
+            [
+                1,1,1,1,1,1,1,1,1,1,
+                1,1,1,1,1,1,1,1,1,1,
+                1,1,4,1,4,1,1,1,1,1,
+                1,1,1,1,1,1,1,1,1,1,
+                1,1,1,1,1,1,1,1,1,1,
+                1,1,1,1,1,1,1,1,1,1,
+                1,1,1,1,1,1,1,1,1,1,
+                1,1,1,1,1,1,2,1,1,1,
+                1,1,1,1,1,1,1,1,1,1,
+                1,1,1,1,1,1,1,1,1,1
+            ],
+            {kind: 'GREY'}
+        );
+        image.getLocalExtrema(
+            {removeClosePoints:3, region:1}
+        ).should.eql([{x:3, y:2, z:4}, {x:6, y:7, z:2}]);
     });
 });
