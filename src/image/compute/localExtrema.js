db8630fb694f3c210c6af69a99e7d0dd2bafbf45
lpatiny <gb252243>
2016-08-10 06:27:27 +0200

    If we consider only the direct neightbours allow to be at 1 pixel of the border

--- src/image/compute/localExtrema.js
+++ src/image/compute/localExtrema.js
@@ -3,7 +3,8 @@
  * Returns an array of object with position.
  * @memberof Image
  * @param mask region of the image that is analyzed. The rest is omitted.
- * @param region 1, 2 or 3. Define the region around each points that is analyzed.
+ * @param region 1, 2 or 3. Define the region around each points that is analyzed. 1 corresponds to 4 cross points, 2 to
+ *        the 8 points around and 3 to the 12 points around the central pixel
  * @param removeClosePoints Remove pts which have a distance between them smaller than this param.
  * @param algorithm chose between min or max local.
  * @returns {[number]} Array having has size the number of channels
@@ -32,9 +33,10 @@ export default function localExtrema (
 
     let dx = [+1, 0, -1, 0, +1, +1, -1, -1, +2, 0, -2, 0, +2, +2, -2, -2];
     let dy = [0, +1, 0, -1, +1, -1, +1, -1, 0, +2, 0, -2, +2, -2, +2, -2];
+    let shift = (region <=8) ? 1 : 2;
     let points = [];
-    for (let currentY = 2; currentY < image.height - 2; currentY++) {
-        for (let currentX = 2; currentX < image.width - 2; currentX++) {
+    for (let currentY = shift; currentY < image.height - shift; currentY++) {
+        for (let currentX = shift; currentX < image.width - shift; currentX++) {
             if (mask && !mask.getBitXY(currentX, currentY)) {
                 continue;
             }
