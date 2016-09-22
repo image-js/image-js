3d815845dd407adeded5bd0b6e23a8e676e4f1e0
lpatiny <luc@patiny.com>
2016-09-22 20:17:12 +0200

    Some work on the docs

--- src/image/filter/invert.js
+++ src/image/filter/invert.js
@@ -3,12 +3,22 @@
 import {validateArrayOfChannels} from '../../util/channel';
 
 /**
+ * Invert an image. The image
  * @memberof Image
  * @instance
+ * @param {object} options
+ * @param {(undefined|number|string|[number]|[string])} [options.channels=undefined] Specify which channels should be processed
+ *      * undefined : we take all the channels but alpha
+ *      * number : this specific channel
+ *      * string : converted to a channel based on rgb, cmyk, hsl or hsv (one letter code)
+ *      * [number] : array of channels as numbers
+ *      * [string] : array of channels as one letter string
  * @returns {this}
  */
 
-export default function invert({channels} = {}) {
+export default function invert(options = {}) {
+    let {channels} = options;
+
     this.checkProcessable('invertOneLoop', {
         bitDepth: [1, 8, 16]
     });
@@ -22,16 +32,12 @@ export default function invert({channels} = {}) {
             data[i] = ~data[i];
         }
     } else {
-        channels = validateArrayOfChannels(this, channels, true);
-
-        let data = this.data;
-
-        // for (let j of channels) { WOULD SLOW DO OF A FACTOR 10 !
+        channels = validateArrayOfChannels(this, {channels});
 
         for (let c = 0; c < channels.length; c++) {
             let j = channels[c];
-            for (let i = j; i < data.length; i += this.channels) {
-                data[i] = this.maxValue - data[i];
+            for (let i = j; i < this.data.length; i += this.channels) {
+                this.data[i] = this.maxValue - this.data[i];
             }
         }
     }
