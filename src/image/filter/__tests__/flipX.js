fcc9b6365bbd1b9cd9f7d32965adb5bc45c332da
Zoé Baraschi <zoe.baraschi@epfl.ch>
2016-10-11 14:58:22 +0200

    modified test values for -should flip pixels horizontally of all RGBA components for a [2,1] image- test and added new test -should flip pixels horizontally of all RGBA components for a [2,2] image-

--- src/image/filter/__tests__/flipX.js
+++ src/image/filter/__tests__/flipX.js
@@ -1,12 +1,20 @@
 import {Image} from 'test/common';
 
 describe('flipX', function () {
-    it('should flip pixels horizontally of all RGBA components', function () {
+	it('should flip pixels horizontally of all RGBA components for a [2,1] image', function () {
 
-        let image = new Image(2, 1, [0, 0, 0, 255, 255, 255, 255, 255]);
+		let image = new Image(2,1,[1, 2, 3, 4, 5, 6, 7, 8]);
 
-        let flipped = [255, 255, 255, 255, 0, 0, 0, 255];
+		let flipped = [5, 6, 7, 8, 1, 2, 3, 4];
 
+		image.flipX();
+		image.data.should.eql(flipped);
+	});
+	it('should flip pixels horizontally of all RGBA components for a [2,2] image', function () {
+
+		let image = new Image(2,2,[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]);
+
+		let flipped = [5, 6, 7, 8, 1, 2, 3, 4, 13, 14, 15, 16, 9, 10, 11, 12];
         image.flipX();
         image.data.should.eql(flipped);
     });
@@ -17,8 +25,7 @@ describe('flipX', function () {
 			{kind: 'GREY'});
 
         let flipped = [2, 1, 4, 3];
-
-        image.flipX();
+		image.flipX();
         image.data.should.eql(flipped);
     });
 });
