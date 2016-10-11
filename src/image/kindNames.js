3ba7104d55581f41c4e25238941be42ddfed489f
Luc Patiny <luc@patiny.com>
2016-10-11 15:13:46 +0200

    Add CMYK and CMYKA kinds

--- src/image/kindNames.js
+++ src/image/kindNames.js
@@ -1,8 +1,10 @@
 // Shortcuts for common image kinds
 
 export const BINARY = 'BINARY';
-export const GREYA = 'GREYA';
-export const RGBA = 'RGBA';
-export const RGB = 'RGB';
 export const GREY = 'GREY';
+export const GREYA = 'GREYA';
+export const RGB = 'RGB';
+export const RGBA = 'RGBA';
+export const CMYK = 'CMYK';
+export const CMYKA = 'CMYKA';
 
