ad834bad97fb70ff3c2fb3f719bcfb343b849057
Zoé Baraschi <zoe.baraschi@epfl.ch>
2016-10-11 15:04:33 +0200

    added test should flip pixels horizontally of all RGBA components for a [3,2] image, for odd numbered pixels

--- src/image/filter/__tests__/flipX.js
+++ src/image/filter/__tests__/flipX.js
@@ -19,9 +19,21 @@ describe('flipX', function () {
         image.data.should.eql(flipped);
     });
 
-    it('should flip pixels horizontally of GREY image', function () {
+	it('should flip pixels horizontally of all RGBA components for a [3,2] image', function () {
 
-        let image = new Image(2, 2, [1, 2, 3, 4],
+		let image = new Image(3,2,[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+			                       13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]);
+
+		let flipped = [9, 10, 11, 12, 5, 6, 7, 8, 1, 2, 3, 4,
+			           21, 22, 23, 24, 17, 18, 19, 20, 13, 14, 15, 16];
+
+		image.flipX();
+		image.data.should.eql(flipped);
+	});
+
+	it('should flip pixels horizontally of GREY image', function () {
+
+		let image = new Image(2,2, [1, 2, 3, 4],
 			{kind: 'GREY'});
 
         let flipped = [2, 1, 4, 3];
