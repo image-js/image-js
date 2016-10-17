4715d39f7d847cbc84c856433db7d893dc184239
Zoé Baraschi <zoe.baraschi@epfl.ch>
2016-10-17 15:55:15 +0200

    FlipY and tests for it (#253)
    * flip image on y axis and test
    
    * added return this and removed some spaces

--- src/image/filter/flipY.js
+++ src/image/filter/flipY.js
@@ -7,17 +7,24 @@
 
 export default function flipY() {
     this.checkProcessable('flipY', {
-        bitDepth: [1, 8, 16]
+        bitDepth: [8, 16]
     });
 
-    for (let x = this.width - 1; x >= 0; x--) {
-        for (let aY = 0, bY = this.height - 1; aY <= bY; aY++, bY--) {
-            let pixelA = this.getPixelXY(x, aY);
-            let pixelB = this.getPixelXY(x, bY);
-            this.setPixelXY(x, bY, pixelA);
-            this.setPixelXY(x, aY, pixelB);
+
+    for (let i = 0; i < Math.floor(this.height / 2); i++) {
+
+        for (let j = 0; j < this.width; j++) {
+            let posCurrent = j * this.channels + (i * this.width * this.channels);
+            let posOpposite =  j * this.channels + (this.height - 1 - i) * this.channels * this.width;
+
+            for (let k = 0; k < this.channels; k++) {
+                let tmp = this.data[posCurrent + k];
+                this.data[posCurrent + k] = this.data[posOpposite + k];
+                this.data[posOpposite + k] = tmp;
+            }
         }
     }
 
     return this;
 }
+
