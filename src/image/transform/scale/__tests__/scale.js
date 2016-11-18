6976c11bef1ae078b2a14e79fb8c0db31fddcec2
Michaël Zasso <targos@protonmail.com>
2016-11-18 13:26:24 +0100

    feat(scale): add preserveAspectRatio option
    It is true by default.
    Closes: #259

---
+++ src/image/transform/scale/__tests__/scale.js
@@ -0,0 +1,107 @@
+import {Image} from 'test/common';
+
+describe('scale', function () {
+    describe('using a factor', function () {
+        it('up - even dimensions', function () {
+            const image = new Image(10, 12);
+            const result = image.scale({factor: 2});
+            result.width.should.equal(20);
+            result.height.should.equal(24);
+        });
+
+        it('down - even dimensions', function () {
+            const image = new Image(10, 12);
+            const result = image.scale({factor: 0.5});
+            result.width.should.equal(5);
+            result.height.should.equal(6);
+        });
+
+        it('up - decimal factor', function () {
+            const image = new Image(9, 13);
+            const result = image.scale({factor: 1.5});
+            result.width.should.equal(14);
+            result.height.should.equal(20);
+        });
+
+        it('down - odd dimensions', function () {
+            const image = new Image(9, 13);
+            const result = image.scale({factor: 1/3});
+            result.width.should.equal(3);
+            result.height.should.equal(4);
+        });
+    });
+
+    describe('with preserveAspectRatio', function () {
+        it('up - auto height', function () {
+            const image = new Image(10, 12);
+            const result = image.scale({width: 20});
+            result.width.should.equal(20);
+            result.height.should.equal(24);
+        });
+
+        it('up - auto width', function () {
+            const image = new Image(10, 12);
+            const result = image.scale({height: 20});
+            result.width.should.equal(17);
+            result.height.should.equal(20);
+        });
+
+        it('down - auto height', function () {
+            const image = new Image(10, 12);
+            const result = image.scale({width: 5});
+            result.width.should.equal(5);
+            result.height.should.equal(6);
+        });
+
+        it('down - auto width', function () {
+            const image = new Image(10, 12);
+            const result = image.scale({height: 5});
+            result.width.should.equal(4);
+            result.height.should.equal(5);
+        });
+    });
+
+    describe('without preserveAspectRatio', function () {
+        it('up - auto height', function () {
+            const image = new Image(10, 12);
+            const result = image.scale({width: 20, preserveAspectRatio: false});
+            result.width.should.equal(20);
+            result.height.should.equal(12);
+        });
+
+        it('up - auto width', function () {
+            const image = new Image(10, 12);
+            const result = image.scale({height: 20, preserveAspectRatio: false});
+            result.width.should.equal(10);
+            result.height.should.equal(20);
+        });
+
+        it('down - auto height', function () {
+            const image = new Image(10, 12);
+            const result = image.scale({width: 5, preserveAspectRatio: false});
+            result.width.should.equal(5);
+            result.height.should.equal(12);
+        });
+
+        it('down - auto width', function () {
+            const image = new Image(10, 12);
+            const result = image.scale({height: 5, preserveAspectRatio: false});
+            result.width.should.equal(10);
+            result.height.should.equal(5);
+        });
+
+        it('with up factor', function () {
+            const image = new Image(10, 12);
+            const result = image.scale({factor: 2, height: 5, preserveAspectRatio: false});
+            result.width.should.equal(20);
+            result.height.should.equal(10);
+        });
+
+        it('with down factor', function () {
+            const image = new Image(10, 12);
+            const result = image.scale({factor: 0.5, height: 5, preserveAspectRatio: false});
+            result.width.should.equal(5);
+            result.height.should.equal(3);
+        });
+    });
+});
--- src/image/transform/scale/scale.js
+++ src/image/transform/scale/scale.js
@@ -12,28 +12,46 @@ import {factorDimensions} from '../../../util/converter';
  * @param {number} [options.height=this.height] - new height
  * @param {number} [options.factor=1] - scaling factor (applied to the new width and height values)
  * @param {string} [options.algorithm='nearestNeighbor']
+ * @param {boolean} [options.preserveAspectRatio=false] - preserve width/height ratio if only one of them is defined
  * @return {Image}
  */
 export default function scale(options = {}) {
     const {
-        width = this.width,
-        height = this.height,
         factor = 1,
-        algorithm = 'nearestNeighbor'
+        algorithm = 'nearestNeighbor',
+        preserveAspectRatio = true
     } = options;
 
-    const {width: newWidth, height: newHeight} = factorDimensions(factor, width, height);
+    let width = options.width;
+    let height = options.height;
 
-    if (newWidth === this.width && newHeight === this.height) {
+    if (!width) {
+        if (height && preserveAspectRatio) {
+            width = Math.round(height * this.width / this.height);
+        } else {
+            width = this.width;
+        }
+    }
+    if (!height) {
+        if (preserveAspectRatio) {
+            height = Math.round(width * this.height / this.width);
+        } else {
+            height = this.height;
+        }
+    }
+
+    ({width, height} = factorDimensions(factor, width, height));
+
+    if (width === this.width && height === this.height) {
         return this.clone();
     }
 
-    const newImage = Image.createFrom(this, {width: newWidth, height: newHeight});
+    const newImage = Image.createFrom(this, {width, height});
 
     switch (algorithm.toLowerCase()) {
         case 'nearestneighbor':
         case 'nearestneighbour':
-            nearestNeighbor.call(this, newImage, newWidth, newHeight);
+            nearestNeighbor.call(this, newImage, width, height);
             break;
         default:
             throw new Error('Unsupported scale algorithm: ' + algorithm);
