64412fd24b0c1f6bb8d4189ca3a11d93000e20c6
Luc Patiny <luc@patiny.com>
2016-11-21 08:26:22 +0100

    Add minimal size of resulting image to 1,1 when scalling binary to much.

--- src/image/transform/__tests__/resizeBinary.js
+++ src/image/transform/__tests__/resizeBinary.js
@@ -25,4 +25,16 @@ describe('Can we resize a binary image', function () {
         Array.from(newImage.data).should.eql([144]);
         newImage.position.should.eql([1, 1]);
     });
+
+    it.only('check the result fi size should be 0', function () {
+        let binary = new Image(4, 4, [204, 51], {
+            kind: 'BINARY'
+        });
+        let newImage = binary.resizeBinary(0.01);
+        newImage.width.should.equal(1);
+        newImage.height.should.equal(1);
+        Array.from(newImage.data).should.eql([128]);
+        newImage.position.should.eql([2, 2]);
+    });
+    
 });
