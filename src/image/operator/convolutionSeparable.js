a63c7031c734bc45495162bbb2f90c4b3c7390ae
Michaël Zasso <targos@protonmail.com>
2017-05-22 17:21:01 +0200

    reuse arrays for separable convolution

--- src/image/operator/convolutionSeparable.js
+++ src/image/operator/convolutionSeparable.js
@@ -3,22 +3,27 @@ import {directConvolution} from 'ml-convolution';
 export default function convolutionSeparable(data, kernel, width, height) {
     const result = new Array(data.length);
     const offset = (kernel.length - 1) / 2;
+    let tmp, conv;
+
+    conv = new Array(width + kernel.length - 1);
+    tmp = new Array(width);
     for (let y = 0; y < height; y++) {
-        const tmp = new Array(width);
         for (let x = 0; x < width; x++) {
             tmp[x] = data[y * width + x];
         }
-        const conv = directConvolution(tmp, kernel);
+        directConvolution(tmp, kernel, conv);
         for (let x = 0; x < width; x++) {
             result[y * width + x] = conv[offset + x];
         }
     }
+
+    conv = new Array(height + kernel.length - 1);
+    tmp = new Array(height);
     for (let x = 0; x < width; x++) {
-        const tmp = new Array(height);
         for (let y = 0; y < height; y++) {
             tmp[y] = result[y * width + x];
         }
-        const conv = directConvolution(tmp, kernel);
+        directConvolution(tmp, kernel, conv);
         for (let y = 0; y < height; y++) {
             result[y * width + x] = conv[offset + y];
         }
