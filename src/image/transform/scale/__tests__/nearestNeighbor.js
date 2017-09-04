0e6816e44934396127275b3e96a098129ccfdb2b
Michaël Zasso <targos@protonmail.com>
2017-09-04 20:36:23 +0200

    fix nearestNeighbor test for jest update

--- src/image/transform/scale/__tests__/nearestNeighbor.js
+++ src/image/transform/scale/__tests__/nearestNeighbor.js
@@ -2,25 +2,19 @@ import {load, getHash} from 'test/common';
 import 'should';
 
 describe('Scale - nearest neighbor', function () {
-    it('check images resize', function () {
-        let original;
-        beforeEach(function () {
-            return load('resize/5x5a_5.png').then(function (img) {
-                original = img;
-            });
-        });
-
-        const tests = [1, 2, 3, 4, 5, 10, 13, 18];
-        tests.forEach(function (test) {
-            it(`5x5 -> ${test}x${test}`, function () {
-                return load('resize/5x5a_' + test + '.png').then(function (expected) {
-                    getHash(original.scale({
-                        height: test,
-                        width: test
-                    })).should.equal(getHash(expected));
-                });
-            });
-        });
+    let original;
+    beforeEach(async () => {
+        original = await load('resize/5x5a_5.png');
     });
 
+    const tests = [1, 2, 3, 4, 5, 10, 13, 18];
+    tests.forEach(function (test) {
+        it(`5x5 -> ${test}x${test}`, async () => {
+            const expected = await load(`resize/5x5a_${test}.png`);
+            getHash(original.scale({
+                height: test,
+                width: test
+            })).should.equal(getHash(expected));
+        });
+    });
 });
