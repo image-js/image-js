fb4cbaf8f99f6f696357f5de24a60fe60a1c7f61
Daniel Kostro <kostro.d@gmail.com>
2018-01-31 15:07:17 +0100

    fix binary tests

--- src/__tests__/test-binary.js
+++ src/__tests__/test-binary.js
@@ -8,13 +8,13 @@ test('test-binary', () => {
       0101
       0101
   `;
-    expect(result).toEqual([0b10101010, 0b01010101]);
+    expect(result).toEqual(new Uint8Array([0b10101010, 0b01010101]));
   }
   {
     const result = binary`
       1010101001010101
     `;
-    expect(result).toEqual([0b10101010, 0b01010101]);
+    expect(result).toEqual(new Uint8Array([0b10101010, 0b01010101]));
   }
 
   {
@@ -23,6 +23,6 @@ test('test-binary', () => {
       00000000
       00001111
     `;
-    expect(result).toEqual([0b11111111, 0b00000000, 0b00001111]);
+    expect(result).toEqual(new Uint8Array([0b11111111, 0b00000000, 0b00001111]));
   }
 });
