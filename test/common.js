87ec4b64ab2a9b6e536a769a4c8180ad046283a6
Michaël Zasso <targos@protonmail.com>
2018-01-31 15:37:27 +0100

    apply eslint to common.js

--- test/common.js
+++ test/common.js
@@ -1,43 +1,45 @@
-import {sync as rmSync} from 'rimraf';
-import {mkdirSync} from 'fs';
-import {getImage} from './test-util';
+import { mkdirSync } from 'fs';
 
-export {getImage, getHash} from './test-util';
+import { sync as rmSync } from 'rimraf';
 
 import Image from '../src/image/Image';
 import Stack from '../src/stack/Stack';
 import IJS from '../src/index';
 
-export {Image, Stack, IJS};
+import { getImage, getHash } from './test-util';
+
+export { getImage, getHash };
+
+export { Image, Stack, IJS };
 
 export function load(name) {
-    return Image.load(getImage(name));
+  return Image.load(getImage(name));
 }
 
 export function getSquare() {
-    return new Image(3,3,[
-        0,  0,  255,255, // red
-        0,  255,0,  255, // green
-        255,0,  0,  255, // blur
-        255,255,0,  255, // yellow
-        255,0,  255,255, // magenta
-        0,  255,255,255, // cyan
-        0,  0,  0,  255, // black
-        255,255,255,255, // white
-        127,127,127,255  // grey
-    ]);
+  return new Image(3, 3, [
+    0,  0,  255, 255, // red
+    0,  255, 0,  255, // green
+    255, 0,  0,  255, // blur
+    255, 255, 0,  255, // yellow
+    255, 0,  255, 255, // magenta
+    0,  255, 255, 255, // cyan
+    0,  0,  0,  255, // black
+    255, 255, 255, 255, // white
+    127, 127, 127, 255  // grey
+  ]);
 }
 
 export function get1BitSquare() {
-    return new Image(3,3, new Uint8Array([
-       0b00001000, 0b00000000
-    ]), {
-        kind: 'BINARY'
-    });
+  return new Image(3, 3, new Uint8Array([
+    0b00001000, 0b00000000
+  ]), {
+    kind: 'BINARY'
+  });
 }
 
-export let tmpDir = __dirname + '/TMP';
+export let tmpDir = `${__dirname}/TMP`;
 export function refreshTmpDir() {
-    rmSync(tmpDir);
-    mkdirSync(tmpDir);
+  rmSync(tmpDir);
+  mkdirSync(tmpDir);
 }
