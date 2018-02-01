37948e84e9fb1e67650abd4d06743b721cfa735f
Michaël Zasso <targos@protonmail.com>
2018-02-01 10:28:06 +0100

    fix browser build

--- rollup.config.js
+++ rollup.config.js
@@ -3,41 +3,34 @@ import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
 import resolve from 'rollup-plugin-node-resolve';
 
-export default {
-    input: 'src/index.js',
-    output: {
-        file: 'dist/image.js',
-        format: 'umd',
-        name: 'IJS'
-    },
-    plugins: [
-        babel({
-            exclude: 'node_modules/**'
-        }),
-        resolve({
-            module: true,
-            main: true,
-            browser: true,
-            extensions: ['.js', '.json']
-        }),
-        commonjs({
-            namedExports: {
-                'node_modules/blob-util/lib/index.js': ['canvasToBlob'],
-                'node_modules/color-functions/index.js': ['cssColor'],
-                'node_modules/jpeg-js/index.js': ['decode', 'encode'],
-                'node_modules/ml-matrix-convolution/src/index.js': [
-                    'direct',
-                    'fft'
-                ],
-                'node_modules/ml-array-utils/src/index.js': ['scale'],
-                'node_modules/pako/index.js': [
-                    'inflate',
-                    'Inflate',
-                    'deflate',
-                    'Deflate'
-                ]
-            }
-        }),
-        json()
-    ]
+const config = {
+  input: 'src/index.js',
+  output: {
+    exports: 'named',
+    file: 'dist/image.js',
+    format: 'umd',
+    name: 'IJS'
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    resolve({
+      module: true,
+      main: true,
+      browser: true,
+      extensions: ['.js', '.json']
+    }),
+    commonjs({
+      namedExports: {
+        'node_modules/pako/index.js': [
+          'Inflate',
+          'deflate',
+        ]
+      }
+    }),
+    json()
+  ]
 };
+
+export default config;
