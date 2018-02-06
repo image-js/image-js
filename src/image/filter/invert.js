9de41d08d838b72bedc7981074a6232c7ad5672e
Michaël Zasso <targos@protonmail.com>
2018-02-06 17:08:51 +0100

    simplify invert

--- src/image/filter/invert.js
+++ src/image/filter/invert.js
@@ -1,16 +1,10 @@
-import { validateArrayOfChannels } from '../../util/channel';
-
 /**
  * Invert the colors of an image
  * @memberof Image
  * @instance
- * @param {object} [options]
- * @param {SelectedChannels} [options.channels=undefined]
  * @return {Image}
  */
-export default function invert(options = {}) {
-  let { channels } = options;
-
+export default function invert() {
   this.checkProcessable('invert', {
     bitDepth: [1, 8, 16]
   });
@@ -18,24 +12,21 @@ export default function invert(options = {}) {
   if (this.bitDepth === 1) {
     invertBinary(this);
   } else {
-    invertColor(this, channels);
+    invertColor(this);
   }
   return this;
 }
 
 function invertBinary(image) {
-  const data = image.data;
-  for (let i = 0; i < data.length; i++) {
-    data[i] = ~data[i];
+  for (let i = 0; i < image.data.length; i++) {
+    image.data[i] = ~image.data[i];
   }
 }
 
-function invertColor(image, channels) {
-  channels = validateArrayOfChannels(image, { channels });
-  for (let c = 0; c < channels.length; c++) {
-    let j = channels[c];
-    for (let i = j; i < image.data.length; i += image.channels) {
-      image.data[i] = image.maxValue - image.data[i];
+function invertColor(image) {
+  for (let pixel = 0; pixel < image.data.length; pixel += image.channels) {
+    for (let c = 0; c < image.components; c++) {
+      image.data[pixel + c] = image.maxValue - image.data[pixel + c];
     }
   }
 }
