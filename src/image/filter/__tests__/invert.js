e2af94c5ef4340bc593686272b4dc6f88230e106
Michaël Zasso <targos@protonmail.com>
2018-02-06 16:44:03 +0100

    refactor filter test

--- src/image/filter/__tests__/invert.js
+++ src/image/filter/__tests__/invert.js
@@ -4,43 +4,41 @@ describe('invert', function () {
   it('should invert colors of 3 components of RGBA, not alpha', function () {
     let image = new Image(1, 2, [230, 83, 120, 255, 100, 140, 13, 240]);
 
-    let inverted = [25, 172, 135, 255, 155, 115, 242, 240];
+    let expected = [25, 172, 135, 255, 155, 115, 242, 240];
 
-    image.invert();
-    expect(image.data).toEqual(inverted);
+    const inverted = image.invert();
+    expect(inverted).toBe(image);
+    expect(Array.from(inverted.data)).toEqual(expected);
   });
 
   it('should invert grey of GREY image', function () {
-    let image = new Image(2, 2, [1, 2, 3, 4],
-      { kind: 'GREY' });
+    let image = new Image(2, 2, [1, 2, 3, 4], { kind: 'GREY' });
 
-    let inverted = [254, 253, 252, 251];
+    let expected = [254, 253, 252, 251];
 
-    image.invert();
-    expect(image.data).toEqual(inverted);
+    const inverted = image.invert();
+    expect(inverted).toBe(image);
+    expect(Array.from(inverted.data)).toEqual(expected);
   });
 
   it('should invert grey 16 bits of GREY image', function () {
-    let image = new Image(2, 2, [1, 2, 3, 4],
-      { kind: 'GREY', bitDepth: 16 });
+    let image = new Image(2, 2, [1, 2, 3, 4], { kind: 'GREY', bitDepth: 16 });
 
-    let inverted = [65534, 65533, 65532, 65531];
+    let expected = [65534, 65533, 65532, 65531];
 
-    image.invert();
-    expect(image.data).toEqual(inverted);
+    const inverted = image.invert();
+    expect(inverted).toBe(image);
+    expect(Array.from(inverted.data)).toEqual(expected);
   });
 
   it('should invert data if BINARY image', function () {
-    let data = new Uint8Array(1);
-    data[0] = 85;
+    let data = Uint8Array.of(85);
+    let image = new Image(2, 4, data, { kind: 'BINARY' });
 
-    let image = new Image(2, 4, data,
-      { kind: 'BINARY' });
+    let expected = Uint8Array.of(170);
 
-    let inverted = new Uint8Array(1);
-    inverted[0] = [170];
-
-    image.invert();
-    expect(image.data).toEqual(inverted);
+    const inverted = image.invert();
+    expect(inverted).toBe(image);
+    expect(inverted.data).toEqual(expected);
   });
 });
