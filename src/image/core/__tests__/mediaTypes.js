2403a8e557f4aee68df4efcfc48494d917b00f9e
Michaël Zasso <targos@protonmail.com>
2018-02-07 21:20:33 +0100

    remove unused mediatype methods

--- src/image/core/__tests__/mediaTypes.js
+++ src/image/core/__tests__/mediaTypes.js
@@ -1,34 +1,6 @@
-import { Image } from 'test/common';
+import { getType } from '../mediaTypes';
 
-import { getType, canWrite } from '../mediaTypes';
-
-describe('Media Type support checks', function () {
-
-  it('getType', function () {
-    expect(getType('png')).toBe('image/png');
-    expect(getType('image/png')).toBe('image/png');
-  });
-
-  it('direct calls', function () {
-    expect(canWrite('image/png')).toBe(true);
-    expect(canWrite('image/jpeg')).toBe(false);
-    expect(canWrite('image/abc')).toBe(false);
-  });
-
-  it('static method', function () {
-    expect(Image.isTypeSupported('png')).toBe(true);
-    expect(Image.isTypeSupported('image/png')).toBe(true);
-    expect(Image.isTypeSupported('image/jpeg')).toBe(false);
-    expect(Image.isTypeSupported('abc')).toBe(false);
-  });
-
-  it('static method - bad arguments', function () {
-    expect(function () {
-      Image.isTypeSupported('', 'other');
-    }).toThrowError(/unknown operation/);
-    expect(function () {
-      Image.isTypeSupported(123);
-    }).toThrowError(TypeError);
-  });
-
+test('getType', function () {
+  expect(getType('png')).toBe('image/png');
+  expect(getType('image/png')).toBe('image/png');
 });
--- src/image/core/kindNames.js
+++ src/image/core/kindNames.js
@@ -7,4 +7,3 @@ export const RGB = 'RGB';
 export const RGBA = 'RGBA';
 export const CMYK = 'CMYK';
 export const CMYKA = 'CMYKA';
-
