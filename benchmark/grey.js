587aa14cc5ee01d9b5588ccf38c31d200d872670
Luc Patiny <luc@patiny.com>
2018-09-16 19:19:48 +0200

    update grey benchmark and add esm

--- benchmark/grey.js
+++ benchmark/grey.js
@@ -1,38 +1,39 @@
 'use strict';
 
-var Benchmark = require('benchmark');
-var Image = require('..');
-var Test = require('../test/test');
+let Benchmark = require('benchmark');
 
-var suite = new Benchmark.Suite;
+let { Image } = require('../src');
+let Test = require('../test/test-util');
 
-var filename='cells/cells.jpg';
+let suite = new Benchmark.Suite();
 
+let filename = 'cells/cells.jpg';
 
 // TODO Order of testing is EXTREMELY important !!!!!
 // So basically this test is really useless ...
 
-Image.load(Test.getImage(filename)).then(function (img) {
+Image.load(Test.getImage(filename))
+  .then(function (img) {
     suite
-        .add('yellow', function () {
-            img.grey({algorithm:'luma709'});
-        })
-        .add('grey', function () {
-            img.grey({algorithm:'luma709'});
-        })
-        .add('red', function () {
-            img.grey({algorithm:'red'});
-        })
-        
-        // .add('grey lightness', function () {
-        //     img.grey({algorithm:'lightness'});
-        // })
-        .on('cycle', function (event) {
-            console.log(String(event.target));
-        })
-        .on('complete', function () {
-            console.log('Fastest is ' + this.filter('fastest').map('name'));
-        })
-        .run();
+      .add('yellow', function () {
+        img.grey({ algorithm: 'luma709' });
+      })
+      .add('grey', function () {
+        img.grey({ algorithm: 'luma709' });
+      })
+      .add('red', function () {
+        img.grey({ algorithm: 'red' });
+      })
 
-}).catch(console.error);
+      // .add('grey lightness', function () {
+      //     img.grey({algorithm:'lightness'});
+      // })
+      .on('cycle', function (event) {
+        console.log(String(event.target));
+      })
+      .on('complete', function () {
+        console.log(`Fastest is ${this.filter('fastest').map('name')}`);
+      })
+      .run();
+  })
+  .catch(console.error);
