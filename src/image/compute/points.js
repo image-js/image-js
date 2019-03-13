db41f62af5a691ca08b5951c3e4c7b9c15e01446
Michaël Zasso <targos@protonmail.com>
2019-03-13 14:55:38 +0100

    refactor: use array#push instead of resizing

--- src/image/compute/points.js
+++ src/image/compute/points.js
@@ -9,15 +9,13 @@ export default function points() {
     bitDepth: [1]
   });
 
-  let pixels = new Array(this.size);
-  let counter = 0;
+  const pixels = [];
   for (let x = 0; x < this.width; x++) {
     for (let y = 0; y < this.height; y++) {
       if (this.getBitXY(x, y) === 1) {
-        pixels[counter++] = [x, y];
+        pixels.push([x, y]);
       }
     }
   }
-  pixels.length = counter;
   return pixels;
 }
--- src/image/utility/getPixelsGrid.js
+++ src/image/utility/getPixelsGrid.js
@@ -8,11 +8,7 @@
  * @return {object}
  */
 export default function getPixelsGrid(options = {}) {
-  let {
-    sampling = [10, 10],
-    painted = false,
-    mask
-  } = options;
+  let { sampling = [10, 10], painted = false, mask } = options;
 
   this.checkProcessable('getPixelsGrid', {
     bitDepth: [8, 16],
@@ -25,36 +21,29 @@ export default function getPixelsGrid(options = {}) {
 
   const xSampling = sampling[0];
   const ySampling = sampling[1];
-  const nbSamples = xSampling * ySampling;
 
-  const xyS = new Array(nbSamples);
-  const zS = new Array(nbSamples);
+  const xyS = [];
+  const zS = [];
 
   const xStep = this.width / xSampling;
   const yStep = this.height / ySampling;
   let currentX = Math.floor(xStep / 2);
 
-  let position = 0;
   for (let i = 0; i < xSampling; i++) {
     let currentY = Math.floor(yStep / 2);
     for (let j = 0; j < ySampling; j++) {
       let x = Math.round(currentX);
       let y = Math.round(currentY);
       if (!mask || mask.getBitXY(x, y)) {
-        xyS[position] = [x, y];
-        zS[position] = this.getPixelXY(x, y);
-        position++;
+        xyS.push([x, y]);
+        zS.push(this.getPixelXY(x, y));
       }
       currentY += yStep;
     }
     currentX += xStep;
   }
 
-  // resize arrays if needed
-  xyS.length = position;
-  zS.length = position;
-
-  let toReturn = { xyS, zS };
+  const toReturn = { xyS, zS };
 
   if (painted) {
     toReturn.painted = this.rgba8().paintPoints(xyS);
