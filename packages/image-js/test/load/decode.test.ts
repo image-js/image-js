c6be824f2fa718425be9d9d3f816284c936e3c4a
Daniel Kostro <kostro.d@gmail.com>
2019-03-26 17:01:04 +0100

    fix test not being executed

---
+++ packages/image-js/test/load/decode.test.ts
@@ -0,0 +1,13 @@
+import { readImage } from 'test/readFile';
+import { decode, ColorDepth } from 'ijs';
+
+describe('Load PNG', function () {
+  const tests: [string][] = [['grey8']];
+
+  it.each(tests)('auto decode %s', async (name) => {
+    const buffer = readImage(`${name}.png`);
+    expect(() => decode(buffer)).not.toThrow();
+    const decoded = decode(buffer);
+    expect(decoded.depth).toStrictEqual(ColorDepth.UINT8);
+  });
+});
