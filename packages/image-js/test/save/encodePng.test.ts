689f74c421e87be9ce4535ee6b0dec89474d00c8
Daniel Kostro <kostro.d@gmail.com>
2019-03-27 14:25:14 +0100

    update encode png test

--- packages/image-js/test/save/encodePng.test.ts
+++ packages/image-js/test/save/encodePng.test.ts
@@ -5,6 +5,9 @@ describe('encode PNG', () => {
   it('should encode what it decoded', () => {
     const buffer = readImage('grey8.png');
     const img = decode(buffer);
-    expect(decode(encodePng(img)).data).toStrictEqual(img.data);
+    const imgDup = decode(encodePng(img));
+    expect(imgDup.kind).toStrictEqual(img.kind);
+    expect(imgDup.depth).toStrictEqual(img.depth);
+    expect(imgDup.data).toStrictEqual(img.data);
   });
 });
