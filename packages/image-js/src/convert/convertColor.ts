407658c83fe6b1f16bf000b03f4eeff0a6d9d2b0
Michaël Zasso <targos@protonmail.com>
2019-04-01 11:45:46 +0200

    chore: update fast-png to version 4

--- packages/image-js/src/convert/convertColor.ts
+++ packages/image-js/src/convert/convertColor.ts
@@ -12,7 +12,6 @@ export function convertColor(image: Image, kind: ImageKind): Image {
     throw new Error(`Cannot convert color, image is already ${kind}`);
   }
 
-  // TODO: handle conversion from RGB to GREY
   const canConvertTo = canConvert.get(image.kind);
   if (!canConvertTo || !canConvertTo.includes(kind)) {
     throw new Error(`conversion from ${image.kind} to ${kind} not implemented`);
--- packages/image-js/src/load/decodePng.ts
+++ packages/image-js/src/load/decodePng.ts
@@ -11,25 +11,27 @@ export function decodePng(buffer: ArrayBufferView): Image {
 
   let kind: ImageKind;
   let depth: ColorDepth =
-    png.bitDepth === 16 ? ColorDepth.UINT16 : ColorDepth.UINT8;
+    png.depth === 16 ? ColorDepth.UINT16 : ColorDepth.UINT8;
 
-  switch (png.colourType) {
-    case 0:
+  if (png.palette) {
+    return loadPalettePNG(png);
+  }
+
+  switch (png.channels) {
+    case 1:
       kind = ImageKind.GREY;
       break;
     case 2:
-      kind = ImageKind.RGB;
-      break;
-    case 3:
-      return loadPalettePNG(png);
-    case 4:
       kind = ImageKind.GREYA;
       break;
-    case 6:
+    case 3:
+      kind = ImageKind.RGB;
+      break;
+    case 4:
       kind = ImageKind.RGBA;
       break;
     default:
-      throw new Error(`Unexpected colourType: ${png.colourType}`);
+      throw new Error(`Unexpected number of channels: ${png.channels}`);
   }
   return new Image({
     width: png.width,
@@ -48,17 +50,17 @@ function loadPalettePNG(png: IDecodedPNG): Image {
   }
   const pixels = png.width * png.height;
   const data = new Uint8Array(pixels * 3);
-  const pixelsPerByte = 8 / png.bitDepth;
-  const factor = png.bitDepth < 8 ? pixelsPerByte : 1;
-  const mask = parseInt('1'.repeat(png.bitDepth), 2);
+  const pixelsPerByte = 8 / png.depth;
+  const factor = png.depth < 8 ? pixelsPerByte : 1;
+  const mask = parseInt('1'.repeat(png.depth), 2);
   let dataIndex = 0;
 
   for (let i = 0; i < pixels; i++) {
     const index = Math.floor(i / factor);
     let value = png.data[index];
-    if (png.bitDepth < 8) {
+    if (png.depth < 8) {
       value =
-        (value >>> (png.bitDepth * (pixelsPerByte - 1 - (i % pixelsPerByte)))) &
+        (value >>> (png.depth * (pixelsPerByte - 1 - (i % pixelsPerByte)))) &
         mask;
     }
     const paletteValue = png.palette[value];
