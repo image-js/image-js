f9bc80f76d7d87256592cc14481e2a8e913ff4a9
Michaël Zasso <targos@protonmail.com>
2019-04-02 10:20:58 +0200

    fix getImage and add getTestImage

--- test/index.ts
+++ test/index.ts
@@ -23,10 +23,10 @@ export function decodeImage(name: string): Image {
 export function getImage(
   data: number[][] | number[][][],
   kind: ImageKind,
-  depth: ColorDepth
+  depth: ColorDepth = 8
 ): Image {
-  const width = data.length;
-  const height = data[0].length;
+  const height = data.length;
+  const width = data[0].length;
 
   // @ts-ignore
   const flatData: number[] = data.flat(2);
@@ -42,4 +42,36 @@ export function getImage(
     data: imgData,
     depth
   });
+}
+
+const black = [0, 0, 0];
+const grey = [128, 128, 128];
+const white = [255, 255, 255];
+const red = [255, 0, 0];
+const green = [0, 255, 0];
+const blue = [0, 0, 255];
+const cyan = [0, 255, 255];
+const magenta = [255, 0, 255];
+const yellow = [255, 255, 0];
+
+const testImage = [
+  [white, white, white, white, white, white, white, white],
+  [black, black, black, black, black, black, black, black],
+  [black, red, red, white, white, cyan, cyan, black],
+  [black, red, red, white, white, cyan, cyan, black],
+  [black, green, green, black, black, magenta, magenta, black],
+  [black, green, green, black, black, magenta, magenta, black],
+  [black, blue, blue, grey, grey, yellow, yellow, black],
+  [black, blue, blue, grey, grey, yellow, yellow, black],
+  [black, black, black, black, black, black, black, black],
+  [white, white, white, white, white, white, white, white]
+];
+
+export function getTestImage(kind: ImageKind = ImageKind.RGB): Image {
+  const img = getImage(testImage, ImageKind.RGB);
+  if (kind !== ImageKind.RGB) {
+    return img.convertColor(kind);
+  } else {
+    return img;
+  }
 }
