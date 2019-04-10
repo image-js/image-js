8e24000f6fec936bfacbb9ccd70eb6c511ab9761
Michaël Zasso <targos@protonmail.com>
2019-04-10 08:51:11 +0200

    feat: add readSync and writeSync

--- packages/image-js/src/load/read.ts
+++ packages/image-js/src/load/read.ts
@@ -1,4 +1,4 @@
-import { readFile } from 'fs';
+import { readFile, readFileSync } from 'fs';
 import { promisify } from 'util';
 
 import { Image } from '../Image';
@@ -10,8 +10,16 @@ const readFilePromise = promisify(readFile);
 /**
  * Read an image from the disk.
  * The file format is automatically selected based on the first few bytes.
+ * @param path - The file path to read.
  */
 export async function read(path: string): Promise<Image> {
   const data = await readFilePromise(path);
   return decode(data);
+}
+
+/**
+ * Synchronous version of @see {@link read}.
+ */
+export function readSync(path: string): Image {
+  return decode(readFileSync(path));
 }
--- packages/image-js/src/save/write.ts
+++ packages/image-js/src/save/write.ts
@@ -1,4 +1,4 @@
-import { writeFile } from 'fs';
+import { writeFile, writeFileSync } from 'fs';
 import { extname } from 'path';
 import { promisify } from 'util';
 
@@ -42,33 +42,51 @@ export async function write(
   image: Image,
   options?: IEncodeOptionsPng | IEncodeOptionsJpeg
 ): Promise<void> {
-  let format: ImageFormat;
-  let toWrite: Uint8Array;
+  const toWrite = getDataToWrite(path, image, options);
+  await writeFilePromise(path, toWrite);
+}
 
+/**
+ * Synchronous version of @see {@link write}.
+ */
+export function writeSync(
+  path: string,
+  image: Image,
+  options?: IEncodeOptionsPng | IEncodeOptionsJpeg
+): void {
+  const toWrite = getDataToWrite(path, image, options);
+  writeFileSync(path, toWrite);
+}
+
+function getDataToWrite(
+  path: string,
+  image: Image,
+  options?: IEncodeOptionsPng | IEncodeOptionsJpeg
+): Uint8Array {
+  let format: ImageFormat;
   if (options === undefined) {
     const extension = extname(path)
       .slice(1)
       .toLowerCase();
     if (extension === 'png') {
       format = ImageFormat.png;
-      toWrite = encode(image, { format });
+      return encode(image, { format });
     } else if (extension === 'jpg' || extension === 'jpeg') {
       format = ImageFormat.jpg;
-      toWrite = encode(image, { format });
+      return encode(image, { format });
     } else {
       throw new Error(
         'image format could not be determined from file extension. Please use a supported extension or specify the format option'
       );
     }
   } else if (options.format === ImageFormat.png) {
-    toWrite = encode(image, options);
+    return encode(image, options);
   } else if (
     options.format === ImageFormat.jpg ||
     options.format === ImageFormat.jpeg
   ) {
-    toWrite = encode(image, options);
+    return encode(image, options);
   } else {
     throw new RangeError(`unknown format: ${options.format}`);
   }
-  await writeFilePromise(path, toWrite);
 }
