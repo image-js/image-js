32e1494a6949faf5b663037082441b009cf58f08
Luc Patiny <luc@patiny.com>
2020-06-03 09:55:36 +0200

    chore: fix set / get Matrix

--- src/image/utility/setMatrix.js
+++ src/image/utility/setMatrix.js
@@ -1,3 +1,5 @@
+import { Matrix } from "ml-matrix";
+
 /**
  * We set the data of the image from a matrix. The size of the matrix and the data have to be the same.
  * @memberof Image
@@ -7,28 +9,24 @@
  * @param {number} [options.channel]
  */
 export default function setMatrix(matrix, options = {}) {
+  matrix = new Matrix(matrix);
   let { channel } = options;
-  this.checkProcessable('getMatrix', {
+  this.checkProcessable("getMatrix", {
     bitDepth: [8, 16],
   });
 
   if (channel === undefined) {
     if (this.components > 1) {
       throw new RangeError(
-        'You need to define the channel for an image that contains more than one channel',
+        "You need to define the channel for an image that contains more than one channel"
       );
     }
     channel = 0;
   }
 
-  if (
-    !matrix.length ||
-    !matrix[0].length ||
-    this.width !== matrix.columns ||
-    this.height !== matrix.rows
-  ) {
+  if (this.width !== matrix.columns || this.height !== matrix.rows) {
     throw new RangeError(
-      'The size of the matrix must be equal to the size of the image',
+      "The size of the matrix must be equal to the size of the image"
     );
   }
 
