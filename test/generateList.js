c2a8a01543b3c929bde09db69630c29bbfd0a5a9
Luc Patiny <luc@patiny.com>
2020-08-21 16:30:31 +0200

    chore: fix eslint

--- test/generateList.js
+++ test/generateList.js
@@ -5,18 +5,21 @@ var fs = require('fs');
 var list = [];
 
 function getImages(folder) {
-    var files = fs.readdirSync(__dirname + '/' + folder);
-    files.forEach(function (file) {
-        var path = folder + '/' + file;
-        var stat = fs.statSync(__dirname + '/' + path);
-        if (stat.isDirectory()) {
-            getImages(path);
-        } else if (/\.(jpg|jpeg|tiff|tif|png|gif)$/i.test(file)) {
-            list.push(path.slice(4));
-        }
-    });
+  var files = fs.readdirSync(__dirname + '/' + folder);
+  files.forEach(function (file) {
+    var path = folder + '/' + file;
+    var stat = fs.statSync(__dirname + '/' + path);
+    if (stat.isDirectory()) {
+      getImages(path);
+    } else if (/\.(jpg|jpeg|tiff|tif|png|gif)$/i.test(file)) {
+      list.push(path.slice(4));
+    }
+  });
 }
 
 getImages('img');
 
-fs.writeFileSync(__dirname + '/' + 'imgList.json', JSON.stringify(list, null, 2));
+fs.writeFileSync(
+  __dirname + '/' + 'imgList.json',
+  JSON.stringify(list, null, 2),
+);
