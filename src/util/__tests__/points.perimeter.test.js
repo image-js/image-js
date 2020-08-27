817c8316cc11329ce38a38abeb74334f3dbb4084
Luc Patiny <luc@patiny.com>
2020-08-27 08:51:39 +0200

    test: add more perimeter and surface tests

---
+++ src/util/__tests__/points.perimeter.test.js
@@ -0,0 +1,23 @@
+import { surface, perimeter } from '../points';
+
+describe('utilities for points', function () {
+  it('perimeter diamond', function () {
+    expect(
+      perimeter([
+        [0, 0],
+        [1, 1],
+        [0, 2],
+        [-1, 1],
+      ]),
+    ).toBeCloseTo(Math.sqrt(2) * 4);
+  });
+
+  it('perimeter line', function () {
+    expect(
+      perimeter([
+        [0, 0],
+        [0, 1],
+      ]),
+    ).toBeCloseTo(2);
+  });
+});
---
+++ src/util/__tests__/points.surface.test.js
@@ -0,0 +1,78 @@
+import { surface, perimeter } from '../points';
+
+describe('utilities for points: surface', function () {
+  it('square 1', function () {
+    expect(
+      surface([
+        [0, 0],
+        [0, 1],
+        [1, 1],
+        [1, 0],
+      ]),
+    ).toBe(1);
+  });
+
+  it('square 4', function () {
+    expect(
+      surface([
+        [0, 0],
+        [0, 2],
+        [2, 2],
+        [2, 0],
+      ]),
+    ).toBe(4);
+  });
+
+  it('diamond', function () {
+    expect(
+      surface([
+        [0, 0],
+        [1, 1],
+        [0, 2],
+        [-1, 1],
+      ]),
+    ).toBe(2);
+  });
+
+  it('line', function () {
+    expect(
+      surface([
+        [0, 0],
+        [1, 1],
+      ]),
+    ).toBe(0);
+  });
+
+  it('P shape', () => {
+    expect(
+      surface([
+        [0, 0],
+        [0, 4],
+        [2, 4],
+        [2, 2],
+        [0, 2],
+      ]),
+    ).toBe(4);
+  });
+
+  it('complex shape', () => {
+    /*
+    11000
+    11000
+    11000
+    11111
+    01111
+    */
+    expect(
+      surface([
+        [0, 1],
+        [0, 4],
+        [1, 4],
+        [1, 1],
+        [4, 1],
+        [4, 0],
+        [1, 0],
+      ]),
+    ).toBe(6.5);
+  });
+});
