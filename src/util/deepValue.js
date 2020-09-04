8eed11b4c8021bc51a939de6c852b3878857e76e
Luc Patiny <luc@patiny.com>
2020-09-04 08:22:53 +0200

    fix: deepValue and 0 value

--- src/util/deepValue.js
+++ src/util/deepValue.js
@@ -1,7 +1,7 @@
 export default function deepValue(object, path = '') {
   let parts = path.split('.');
   for (let part of parts) {
-    if (!object[part]) return undefined;
+    if (object[part] === undefined) return undefined;
     object = object[part];
   }
   return object;
