5c1d51ba8d76f6aee57b5415589bc158c665b272
Oceane Patiny <oceane@patiny.com>
2022-01-07 15:45:30 +0100

    chore: copy subtractImage from image-js

---
+++ src/filters/subtractImage.ts
@@ -0,0 +1,47 @@
+/**
+ * Calculate a new image that is the subtraction between the current image and the otherImage.
+ *
+ * @param {Image} otherImage
+ * @param {object} [options={}]
+ * @param {number[]|string[]} [options.channels] - : to which channel to apply the filter. By default all but alpha.
+ * @param {number[]|string[]} [options.absolute=false] - :.take the absolute value of the difference (default minimum=0)
+ * @returns {Image}
+ */
+export default function subtractImage(otherImage, options = {}) {
+  let { channels, absolute = false } = options;
+  this.checkProcessable('subtractImage', {
+    bitDepth: [8, 16],
+  });
+  if (this.width !== otherImage.width || this.height !== otherImage.height) {
+    throw new Error('subtractImage: both images must have the same size');
+  }
+  if (
+    this.alpha !== otherImage.alpha ||
+    this.bitDepth !== otherImage.bitDepth
+  ) {
+    throw new Error(
+      'subtractImage: both images must have the same alpha and bitDepth',
+    );
+  }
+  if (this.channels !== otherImage.channels) {
+    throw new Error(
+      'subtractImage: both images must have the same number of channels',
+    );
+  }
+
+  let newImage = this.clone();
+
+  for (let j = 0; j < channels.length; j++) {
+    let c = channels[j];
+    for (let i = c; i < this.data.length; i += this.channels) {
+      let value = this.data[i] - otherImage.data[i];
+      if (absolute) {
+        newImage.data[i] = Math.abs(value);
+      } else {
+        newImage.data[i] = Math.max(value, 0);
+      }
+    }
+  }
+
+  return newImage;
+}
