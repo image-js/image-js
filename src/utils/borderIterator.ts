2ef333c87c202bb83f02a161207b16e46a73b91b
Oceane Patiny <oceane@patiny.com>
2022-01-14 14:53:03 +0100

    feat: write border pixels iterator

---
+++ src/utils/borderIterator.ts
@@ -0,0 +1,22 @@
+import { Mask, IJS } from '..';
+
+/**
+ * Create function that allows to iterate on the pixels of the border of an image.
+ *
+ * @param image - Image for which to create the border iterator.
+ * @yields - Index of the border pixel.
+ */
+export function* borderIterator(image: IJS | Mask) {
+  for (let col = 0; col < image.width; col++) {
+    yield col;
+  }
+  for (let row = 2; row < image.height; row++) {
+    yield row * image.width - 1;
+  }
+  for (let col = 0; col < image.width; col++) {
+    yield image.width * image.height - col - 1;
+  }
+  for (let row = image.height - 2; row >= 1; row--) {
+    yield row * image.width;
+  }
+}
