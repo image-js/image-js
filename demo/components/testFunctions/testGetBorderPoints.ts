f39702be3f730eda523d0cd3d12dec70de1ccfa3
Oceane Patiny <oceane@patiny.com>
2022-06-03 09:56:42 +0200

    docs(demo): getBorderPoints demo

--- demo/components/testFunctions/testGetBorderPoints.ts
+++ demo/components/testFunctions/testGetBorderPoints.ts
@@ -1,7 +1,23 @@
-import { IJS } from '../../../src';
+import { fromMask, IJS, ImageColorModel, Mask } from '../../../src';
+import { RoiKind } from '../../../src/roi/getRois';
 
-export function testGetBorderPixels(image: IJS): IJS {
-  // TODO: implement this demo
+export function testGetBorderPoints(image: IJS): IJS {
+  const grey = image.convertColor(ImageColorModel.GREY);
+  const mask = grey.threshold();
 
-  return image;
+  const roiMapManager = fromMask(mask);
+
+  const rois = roiMapManager.getRois({ kind: RoiKind.BLACK });
+
+  const roi = rois.sort((a, b) => b.surface - a.surface)[0];
+
+  let points = roi.getBorderPoints();
+
+  let borderMask = Mask.fromPoints(roi.width, roi.height, points);
+
+  return image.paintMask(borderMask, {
+    row: roi.row,
+    column: roi.column,
+    color: [0, 255, 0, 255],
+  });
 }
