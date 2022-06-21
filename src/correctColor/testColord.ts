b15e1ed588283b16fb15ccfc696abacc8fe60c65
Oceane Patiny <oceane@patiny.com>
2022-06-21 09:38:16 +0200

    test(correctColor): update snapshot

--- src/correctColor/testColord.ts
+++
@@ -1,9 +0,0 @@
-import { colord, extend } from 'colord';
-import labPlugin from 'colord/plugins/lab';
-
-extend([labPlugin]);
-
-console.log(colord({ r: 255, g: 0, b: 0 }).toLab());
-
-console.log(colord({ l: 50, a: -128, b: -128 }).toRgb());
-console.log(colord({ l: 50, a: 127, b: 127 }).toRgb());
