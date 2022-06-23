8812bbc75e5da64b54d576a492455bdfd817d1c8
Oceane Patiny <oceane@patiny.com>
2022-06-23 09:28:31 +0200

    fix: adapt setBlendedPixel to work on Mask

--- src/utils/getDefaultColor.ts
+++ src/utils/getDefaultColor.ts
@@ -1,14 +1,15 @@
 import { IJS } from '../IJS';
+import { Mask } from '../Mask';
 
 import { ImageColorModel } from './colorModels';
 
 /**
- * Get black color in image color modal
+ * Get the black colorfor a given color model.
  *
- * @param image - The used image
- * @returns Black color
+ * @param image - The used image.
+ * @returns Black color.
  */
-export function getDefaultColor(image: IJS): number[] {
+export function getDefaultColor(image: IJS | Mask): number[] {
   switch (image.colorModel) {
     case ImageColorModel.GREY:
       return [0];
@@ -18,6 +19,8 @@ export function getDefaultColor(image: IJS): number[] {
       return [0, 0, 0];
     case ImageColorModel.RGBA:
       return [0, 0, 0, image.maxValue];
+    case ImageColorModel.BINARY:
+      return [0];
     default:
       throw new Error(
         `image color model ${image.colorModel} is not compatible`,
