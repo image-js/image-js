aad09199385b30168cac20fba37a102367d6b078
Oceane Patiny <oceane@patiny.com>
2022-06-23 14:07:17 +0200

    refactor: rename deleteDouble to deleteDuplicates

---
+++ src/draw/utils/deleteDuplicates.ts
@@ -0,0 +1,22 @@
+import { Point } from '../drawLineOnMask';
+
+/**
+ * Remove duplicate points from array.
+ *
+ * @param points - Polygon points array.
+ * @returns Cleaned polygon points array.
+ */
+export function deleteDuplicates(points: Point[]): Point[] {
+  const finalPoints: Point[] = [];
+  for (let i = 0; i < points.length; i++) {
+    if (
+      points[i].column === points[(i + 1) % points.length].column &&
+      points[i].row === points[(i + 1) % points.length].row
+    ) {
+      continue;
+    } else {
+      finalPoints.push(points[i]);
+    }
+  }
+  return finalPoints;
+}
