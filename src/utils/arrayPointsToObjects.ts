105c97a8982c0f7f338dd1394e1b4d7ec5fa950c
Oceane Patiny <oceane@patiny.com>
2022-06-28 14:34:38 +0200

    feat(arrayPointsToObjects): points conversion tool

---
+++ src/utils/arrayPointsToObjects.ts
@@ -0,0 +1,16 @@
+import { ArrayPoint, Point } from './types';
+
+/**
+ * Convert object points into array points.
+ *
+ * @param points - Array of points as objects.
+ * @returns Array of points as arrays
+ */
+export function arrayPointsToObjects(points: Point[]): ArrayPoint[] {
+  let result: ArrayPoint[] = [];
+
+  for (let point of points) {
+    result.push([point.column, point.row]);
+  }
+  return result;
+}
