55d84115275d5810cbcd598501d60f8fc133b134
Oceane Patiny <oceane@patiny.com>
2022-06-28 13:48:46 +0200

    test(getConvexHullMask): add function test

---
+++ src/roi/getMask/__tests__/getConvexHullMask.test.ts
@@ -0,0 +1,126 @@
+import { fromMask } from '../../fromMask';
+import { RoiKind } from '../../getRois';
+import { getConvexHullMask } from '../getConvexHullMask';
+
+describe('getConvexHullMask', () => {
+  // the following tests fail because there is a bug with filled polygons
+  it.skip('cross', () => {
+    const mask = testUtils.createMask([
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 1, 0],
+    ]);
+    const roiMapManager = fromMask(mask);
+
+    const roi = roiMapManager.getRois({ kind: RoiKind.WHITE })[0];
+    const roiMask = getConvexHullMask(roi);
+    expect(roiMask).toMatchMaskData([
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 1, 0],
+    ]);
+  });
+  it('cross, not filled', () => {
+    const mask = testUtils.createMask([
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 1, 0],
+    ]);
+    const roiMapManager = fromMask(mask);
+
+    const roi = roiMapManager.getRois({ kind: RoiKind.WHITE })[0];
+    const roiMask = getConvexHullMask(roi, {
+      kind: 'convexHull',
+      filled: false,
+    });
+    expect(roiMask).toMatchMaskData([
+      [0, 1, 0],
+      [1, 0, 1],
+      [0, 1, 0],
+    ]);
+  });
+  it('T', () => {
+    const mask = testUtils.createMask([
+      [1, 1, 1],
+      [0, 1, 0],
+      [0, 1, 0],
+    ]);
+    const roiMapManager = fromMask(mask);
+
+    const roi = roiMapManager.getRois({ kind: RoiKind.WHITE })[0];
+    const roiMask = getConvexHullMask(roi);
+
+    console.log(roiMask);
+
+    expect(roiMask).toMatchMaskData([
+      [1, 1, 1],
+      [0, 1, 0],
+      [0, 1, 0],
+    ]);
+  });
+  it('1 pixel ROI', () => {
+    const mask = testUtils.createMask([
+      [0, 0, 0],
+      [0, 0, 1],
+      [0, 0, 0],
+    ]);
+    const roiMapManager = fromMask(mask);
+
+    const roi = roiMapManager.getRois({ kind: RoiKind.WHITE })[0];
+    const roiMask = getConvexHullMask(roi);
+    expect(roiMask).toMatchMaskData([[1]]);
+  });
+  it('2 pixels ROI', () => {
+    const mask = testUtils.createMask([
+      [0, 0, 0],
+      [0, 1, 1],
+      [0, 0, 0],
+    ]);
+    const roiMapManager = fromMask(mask);
+
+    const roi = roiMapManager.getRois({ kind: RoiKind.WHITE })[0];
+    const roiMask = getConvexHullMask(roi);
+    expect(roiMask).toMatchMaskData([[1, 1]]);
+  });
+  it('5x5 cross, filled = false', () => {
+    const mask = testUtils.createMask([
+      [0, 0, 1, 0, 0],
+      [0, 0, 1, 0, 0],
+      [1, 1, 1, 1, 1],
+      [0, 0, 1, 0, 0],
+      [0, 0, 1, 0, 0],
+    ]);
+    const roiMapManager = fromMask(mask);
+
+    const roi = roiMapManager.getRois({ kind: RoiKind.WHITE })[0];
+    const roiMask = getConvexHullMask(roi, {
+      kind: 'convexHull',
+      filled: false,
+    });
+
+    expect(roiMask).toMatchMaskData([
+      [0, 0, 1, 0, 0],
+      [0, 1, 0, 1, 0],
+      [1, 0, 0, 0, 1],
+      [0, 1, 0, 1, 0],
+      [0, 0, 1, 0, 0],
+    ]);
+  });
+  it('test fill', () => {
+    const mask = testUtils.createMask([
+      [1, 1, 1, 1, 1],
+      [0, 0, 1, 1, 0],
+      [0, 0, 1, 0, 0],
+    ]);
+    const roiMapManager = fromMask(mask);
+
+    const roi = roiMapManager.getRois({ kind: RoiKind.WHITE })[0];
+    const roiMask = getConvexHullMask(roi);
+
+    expect(roiMask).toMatchMaskData([
+      [1, 1, 1, 1, 1],
+      [0, 1, 1, 1, 0],
+      [0, 0, 1, 0, 0],
+    ]);
+  });
+});
