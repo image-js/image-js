ac0182676ace289e29452a275606b14d31b92953
Oceane Patiny <oceane@patiny.com>
2022-06-28 17:14:58 +0200

    fix(getMbrMask): migrating to ts

---
+++ src/roi/getMask/__tests__/minimalBoundingRectangle.js
@@ -0,0 +1,168 @@
+import { toBeDeepCloseTo } from 'jest-matcher-deep-close-to';
+
+import binary from 'test/binary';
+
+import { angle } from '../../../util/points';
+import minimalBoundingRectangle from '../minimalBoundingRectangle';
+
+expect.extend({ toBeDeepCloseTo });
+
+describe('Minimal bounding rectangle', function () {
+  it('should return the minimal bounding box', function () {
+    let image = binary`
+        00000000
+        00011000
+        00011000
+        00111111
+        00111111
+        00011000
+        00011000
+        00000000
+      `;
+
+    const result = minimalBoundingRectangle.call(image);
+    expect(result).toHaveLength(4);
+
+    for (let i = 0; i < 4; i++) {
+      let currentAngle = angle(
+        result[(i + 1) % 4],
+        result[i],
+        result[(i + 2) % 4],
+      );
+      expect(Math.abs(currentAngle)).toBeCloseTo(Math.PI / 2, 1e-6);
+    }
+  });
+
+  it('should return the small bounding box', function () {
+    let image = binary`
+      10000001
+      00011000
+      10011010
+    `;
+
+    const result = minimalBoundingRectangle.call(image);
+
+    expect(result).toStrictEqual([
+      [0, 3],
+      [8, 3],
+      [8, 0],
+      [0, 0],
+    ]);
+  });
+
+  it('should return the small bounding box 2', function () {
+    let image = binary`
+      01000100
+      00011000
+      01011010
+    `;
+
+    const result = minimalBoundingRectangle.call(image);
+    expect(result).toStrictEqual([
+      [1, 3],
+      [7, 3],
+      [7, 0],
+      [1, 0],
+    ]);
+  });
+
+  it('should return the small bounding box diamond', function () {
+    let image = binary`
+      00000100
+      00001110
+      00000100
+      `;
+
+    const result = minimalBoundingRectangle.call(image);
+    expect(result).toBeDeepCloseTo(
+      [
+        [3.5, 1.5],
+        [5.5, 3.5],
+        [7.5, 1.5],
+        [5.5, -0.5],
+      ],
+      6,
+    );
+  });
+
+  it('should return the small bounding box rectangle', function () {
+    let image = binary`
+        00000000
+        00001000
+        00011100
+        00111110
+        00011111
+        00001110
+        00000100
+      `;
+    const result = minimalBoundingRectangle.call(image);
+    expect(result).toBeDeepCloseTo(
+      [
+        [8.5, 4.5],
+        [4.5, 0.5],
+        [1.5, 3.5],
+        [5.5, 7.5],
+      ],
+      6,
+    );
+  });
+
+  it('should return the small bounding box rectangle from points', function () {
+    const result = minimalBoundingRectangle({
+      originalPoints: [
+        [0, 1],
+        [1, 0],
+        [3, 2],
+        [2, 4],
+        [1, 4],
+        [0, 3],
+      ],
+    });
+    expect(result).toBeDeepCloseTo(
+      [
+        [-1, 2],
+        [1, 0],
+        [3.5, 2.5],
+        [1.5, 4.5],
+      ],
+      6,
+    );
+  });
+
+  it('should return the small bouding rectangle for one point', function () {
+    const result = minimalBoundingRectangle({
+      originalPoints: [[2, 2]],
+    });
+    expect(result).toStrictEqual([
+      [2, 2],
+      [2, 2],
+      [2, 2],
+      [2, 2],
+    ]);
+  });
+
+  it('should return the small bouding rectangle for nothing', function () {
+    const result = minimalBoundingRectangle({
+      originalPoints: [],
+    });
+    expect(result).toStrictEqual([]);
+  });
+
+  it('should return the small bouding rectangle for 2 points', function () {
+    const result = minimalBoundingRectangle({
+      originalPoints: [
+        [2, 2],
+        [3, 3],
+      ],
+    });
+    expect(result).toBeDeepCloseTo(
+      [
+        [2, 2],
+        [3, 3],
+        [3, 3],
+        [2, 2],
+      ],
+      6,
+    );
+  });
+});
--- src/roi/getMask/getMbrMask.ts
+++ src/roi/getMask/getMbrMask.ts
@@ -1,9 +1,14 @@
 import { Mask } from '../../Mask';
-import { rotate, difference, normalize } from '../../util/points';
+import {
+  rotate,
+  difference,
+  normalize,
+  Point,
+} from '../../utils/geometry/points';
 import { Roi } from '../Roi';
 import { monotoneChainConvexHull } from '../utils/monotoneChainConvexHull';
 
-export interface ConvexHullMaskOptions {
+export interface MbrMaskOptions {
   kind: 'mbr';
   /**
    * Specify wether the pixels inside the minimum bounding rectangle should be set to 1.
@@ -17,15 +22,28 @@ export interface ConvexHullMaskOptions {
  * Computes the minimum bounding rectangle of an ROI.
  * https://www.researchgate.net/profile/Lennert_Den_Boer2/publication/303783472_A_Fast_Algorithm_for_Generating_a_Minimal_Bounding_Rectangle/links/5751a14108ae6807fafb2aa5.pdf
  *
- * @memberof Image
- * @instance
- * @param roi
- * @param [options]
- * @param [options.originalPoints]
+ * @param roi - The ROI to process.
+ * @param options - Get MBR mask options.
  * @returns A mask with the minimum boundary rectangle of the ROI.
  */
-export function getMbrMask(roi: Roi, options = {}) {
+export function getMbrMask(
+  roi: Roi,
+  options: MbrMaskOptions = { kind: 'mbr' },
+): Mask {
+  const { filled = true } = options;
+
+  return new Mask(1, 1);
+}
+
+/**
+ * Get the four corners of the minimun bounding rectangle of an ROI.
+ *
+ * @param roi - The ROI to process.
+ * @returns The array of corners.
+ */
+export function getMbrCorners(roi: Roi): Point[] {
   const vertices = monotoneChainConvexHull(roi.getBorderPoints());
+
   if (vertices.length === 0) {
     return [];
   }
@@ -34,30 +52,29 @@ export function getMbrMask(roi: Roi, options = {}) {
     return [vertices[0], vertices[0], vertices[0], vertices[0]];
   }
 
-  const p = new Array(vertices.length);
-
+  let corners: Point[] = [];
   let minSurface = Number.POSITIVE_INFINITY;
   let minSurfaceAngle = 0;
   let mbr;
 
-  for (let i = 0; i < p.length; i++) {
-    let angle = getAngle(vertices[i], vertices[(i + 1) % p.length]);
+  for (let i = 0; i < vertices.length; i++) {
+    let angle = getAngle(vertices[i], vertices[(i + 1) % vertices.length]);
 
-    rotate(-angle, vertices, p);
+    corners = rotate(-angle, vertices);
 
     // we rotate and translate so that this axe is in the bottom
-    let aX = p[i][0];
-    let aY = p[i][1];
-    let bX = p[(i + 1) % p.length][0];
-    let bY = p[(i + 1) % p.length][1];
+    let aX = corners[i].column;
+    let aY = corners[i].row;
+    let bX = corners[(i + 1) % corners.length].column;
+    let bY = corners[(i + 1) % corners.length].row;
 
     let tUndefined = true;
     let tMin = 0;
     let tMax = 0;
     let maxWidth = 0;
-    for (let point of p) {
-      let cX = point[0];
-      let cY = point[1];
+    for (let point of corners) {
+      let cX = point.column;
+      let cY = point.row;
       let t = (cX - aX) / (bX - aX);
       if (tUndefined === true) {
         tUndefined = false;
@@ -69,10 +86,12 @@ export function getMbrMask(roi: Roi, options = {}) {
       }
       let width = (-(bX - aX) * cY + bX * aY - bY * aX) / (bX - aX);
 
-      if (Math.abs(width) > Math.abs(maxWidth)) maxWidth = width;
+      if (Math.abs(width) > Math.abs(maxWidth)) {
+        maxWidth = width;
+      }
     }
-    let pMin = [aX + tMin * (bX - aX), aY];
-    let pMax = [aX + tMax * (bX - aX), aY];
+    let minPoint = { column: aX + tMin * (bX - aX), row: aY };
+    let maxPoint = { column: aX + tMax * (bX - aX), row: aY };
 
     let currentSurface = Math.abs(maxWidth * (tMin - tMax) * (bX - aX));
 
@@ -80,20 +99,26 @@ export function getMbrMask(roi: Roi, options = {}) {
       minSurfaceAngle = angle;
       minSurface = currentSurface;
       mbr = [
-        pMin,
-        pMax,
-        [pMax[0], pMax[1] - maxWidth],
-        [pMin[0], pMin[1] - maxWidth],
+        minPoint,
+        maxPoint,
+        { column: maxPoint.column, row: maxPoint.row - maxWidth },
+        { column: minPoint.column, row: minPoint.row - maxWidth },
       ];
     }
   }
-  rotate(minSurfaceAngle, mbr, mbr);
+  const test = rotate(minSurfaceAngle, mbr);
   return mbr;
 }
 
-// the angle that allows to make the line going through p1 and p2 horizontal
-// this is an optimized version because it assume one vector is horizontal
-function getAngle(p1, p2) {
+/**
+ *  The angle that allows to make the line going through p1 and p2 horizontal.
+ *  This is an optimized version because it assumes that one of the vectors is horizontal.
+ *
+ * @param p1 - First point.
+ * @param p2 - Second point.
+ * @returns Rotation angle to make the line horizontal.
+ */
+function getAngle(p1: Point, p2: Point): number {
   let diff = difference(p2, p1);
   let vector = normalize(diff);
   let angle = Math.acos(vector[0]);
