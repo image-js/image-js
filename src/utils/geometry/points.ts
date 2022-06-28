a54a8dd7a9d8a6fad60a0c1c512fdbfbb8004106
Oceane Patiny <oceane@patiny.com>
2022-06-28 17:11:39 +0200

    feat: add points manipulation functions

--- src/utils/geometry/points.ts
+++ src/utils/geometry/points.ts
@@ -1,9 +1,11 @@
 /**
- * Coordinates of a point in an image with the top-left corner being the reference point.
+ * Coordinates of a point in an image with the top-left corner being the reference point. The point is a [column, row] array.
  */
 export type ArrayPoint = [column: number, row: number];
 
-export interface Point {
+/**
+ * Coordinates of a point in an image with the top-left corner being the reference point.
+ */ export interface Point {
   /**
    * Point row.
    *
@@ -14,4 +16,46 @@ export interface Point {
    *
    */
   column: number;
+}
+
+/**
+ * Calculates a new point that is the difference p1 - p2
+ *
+ * @param p1 - First point.
+ * @param p2 - Second Point.
+ * @returns Difference between the two points.
+ */
+export function difference(p1: Point, p2: Point) {
+  return { column: p1.column - p2.column, row: p1.row - p2.row };
+}
+
+/**
+ * Normalize a point (more precisely the vector from the origin to the point).
+ *
+ * @param point - Point to normalise.
+ * @returns - Normalised point.
+ */
+export function normalize(point: Point) {
+  let length = Math.sqrt(point.column ** 2 + point.row ** 2);
+  return [point.column / length, point.row / length];
+}
+
+/**
+ * Rotate an array of points by an angle in radians (counter-clockwise).
+ *
+ * @param radians - Angle in radians.
+ * @param points - Source points
+ * @returns The points after rotation.
+ */
+export function rotate(radians: number, points: Point[]): Point[] {
+  let result: Point[] = [];
+  let cos = Math.cos(radians);
+  let sin = Math.sin(radians);
+  for (let i = 0; i < result.length; ++i) {
+    result[i] = {
+      column: cos * points[i].column - sin * points[i].row,
+      row: sin * points[i].column + cos * points[i].row,
+    };
+  }
+  return result;
 }
