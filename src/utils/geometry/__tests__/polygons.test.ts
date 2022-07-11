036341eb7eead9589f58c5ec5bf1fce84d509cd2
Oceane Patiny <oceane@patiny.com>
2022-07-11 15:01:54 +0200

    feat: implement getPolygonArea

--- src/utils/geometry/__tests__/polygons.test.ts
+++ src/utils/geometry/__tests__/polygons.test.ts
@@ -1,6 +1,6 @@
 import { toBeDeepCloseTo } from 'jest-matcher-deep-close-to';
 
-import { getPolygonPerimeter } from '../polygons';
+import { getPolygonArea, getPolygonPerimeter } from '../polygons';
 
 expect.extend({ toBeDeepCloseTo });
 
@@ -22,5 +22,56 @@ describe('polygons (getPolygonPerimeter)', () => {
     ];
 
     expect(getPolygonPerimeter(points)).toBe(4);
+  });
+});
+describe('polygons (getPolygonArea)', () => {
+  it('area zero', () => {
+    const points = [
+      { column: 0, row: 0 },
+      { column: 0, row: 0 },
+      { column: 0, row: 0 },
+      { column: 0, row: 0 },
+    ];
+
+    expect(getPolygonArea(points)).toBe(0);
+  });
+  it('square', () => {
+    const points = [
+      { column: 0, row: 0 },
+      { column: 1, row: 0 },
+      { column: 1, row: 1 },
+      { column: 0, row: 1 },
+    ];
+
+    expect(getPolygonArea(points)).toBe(1);
+  });
+  it('rectangle', () => {
+    const points = [
+      { column: 0, row: 0 },
+      { column: 3, row: 0 },
+      { column: 3, row: 4 },
+      { column: 0, row: 4 },
+    ];
+
+    expect(getPolygonArea(points)).toBe(12);
+  });
+  it('square with negative values', () => {
+    const points = [
+      { column: -1, row: -1 },
+      { column: 1, row: -1 },
+      { column: 1, row: 1 },
+      { column: -1, row: 1 },
+    ];
+
+    expect(getPolygonArea(points)).toBe(4);
+  });
+  it('triangle', () => {
+    const points = [
+      { column: 0, row: 0 },
+      { column: 3, row: -4 },
+      { column: 6, row: 0 },
+    ];
+
+    expect(getPolygonArea(points)).toBe(12);
   });
 });
--- src/utils/geometry/polygons.ts
+++ src/utils/geometry/polygons.ts
@@ -14,3 +14,21 @@ export function getPolygonPerimeter(points: Point[]): number {
   }
   return perimeter;
 }
+
+/**
+ * Compute the area of a polygon.
+ * Based on the algorithm described on
+ * https://web.archive.org/web/20100405070507/http://valis.cs.uiuc.edu/~sariel/research/CG/compgeom/msg00831.html
+ *
+ * @param points - Array of polygon vertices.
+ * @returns The area.
+ */
+export function getPolygonArea(points: Point[]): number {
+  let area = 0;
+  for (let current = 0; current < points.length; current++) {
+    const next = (current + 1) % points.length;
+    area += points[current].column * points[next].row;
+    area -= points[current].row * points[next].column;
+  }
+  return area / 2;
+}
