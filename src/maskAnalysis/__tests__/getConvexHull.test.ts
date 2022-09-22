530a626420513e40b047f1520335e5ca2f13af76
Oceane Patiny <oceane@patiny.com>
2022-09-22 09:42:31 +0200

    refactor: fix useless describe

--- src/maskAnalysis/__tests__/getConvexHull.test.ts
+++ src/maskAnalysis/__tests__/getConvexHull.test.ts
@@ -4,161 +4,159 @@ import { getConvexHull } from '../getConvexHull';
 
 expect.extend({ toBeDeepCloseTo, toMatchCloseTo });
 
-describe('getConvexHull', () => {
-  it('cross', () => {
-    const mask = testUtils.createMask([
-      [0, 1, 0],
-      [1, 1, 1],
-      [0, 1, 0],
-    ]);
+test('cross', () => {
+  const mask = testUtils.createMask([
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 1, 0],
+  ]);
 
-    const convexHull = getConvexHull(mask);
-    expect(convexHull).toBeDeepCloseTo(
-      {
-        points: [
-          { column: 0, row: 1 },
-          { column: 0, row: 2 },
-          { column: 1, row: 3 },
-          { column: 2, row: 3 },
-          { column: 3, row: 2 },
-          { column: 3, row: 1 },
-          { column: 2, row: 0 },
-          { column: 1, row: 0 },
-        ],
-        surface: 7,
-        perimeter: 9.7,
-      },
-      1,
-    );
-  });
+  const convexHull = getConvexHull(mask);
+  expect(convexHull).toBeDeepCloseTo(
+    {
+      points: [
+        { column: 0, row: 1 },
+        { column: 0, row: 2 },
+        { column: 1, row: 3 },
+        { column: 2, row: 3 },
+        { column: 3, row: 2 },
+        { column: 3, row: 1 },
+        { column: 2, row: 0 },
+        { column: 1, row: 0 },
+      ],
+      surface: 7,
+      perimeter: 9.7,
+    },
+    1,
+  );
+});
 
-  it('small triangle', () => {
-    const mask = testUtils.createMask([
-      [0, 0, 1],
-      [0, 1, 1],
-      [1, 1, 1],
-    ]);
+test('small triangle', () => {
+  const mask = testUtils.createMask([
+    [0, 0, 1],
+    [0, 1, 1],
+    [1, 1, 1],
+  ]);
 
-    const convexHull = getConvexHull(mask);
-    expect(convexHull).toBeDeepCloseTo(
-      {
-        points: [
-          { column: 0, row: 2 },
-          { column: 0, row: 3 },
-          { column: 3, row: 3 },
-          { column: 3, row: 0 },
-          { column: 2, row: 0 },
-        ],
-        surface: 7,
-        perimeter: 10.82,
-      },
-      1,
-    );
-  });
-  it('1 pixel ROI', () => {
-    const mask = testUtils.createMask([
-      [0, 0, 0],
-      [0, 0, 1],
-      [0, 0, 0],
-    ]);
+  const convexHull = getConvexHull(mask);
+  expect(convexHull).toBeDeepCloseTo(
+    {
+      points: [
+        { column: 0, row: 2 },
+        { column: 0, row: 3 },
+        { column: 3, row: 3 },
+        { column: 3, row: 0 },
+        { column: 2, row: 0 },
+      ],
+      surface: 7,
+      perimeter: 10.82,
+    },
+    1,
+  );
+});
+test('1 pixel ROI', () => {
+  const mask = testUtils.createMask([
+    [0, 0, 0],
+    [0, 0, 1],
+    [0, 0, 0],
+  ]);
 
-    const convexHull = getConvexHull(mask);
-    expect(convexHull).toBeDeepCloseTo(
-      {
-        points: [
-          { column: 2, row: 1 },
-          { column: 2, row: 2 },
-          { column: 3, row: 2 },
-          { column: 3, row: 1 },
-        ],
-        surface: 1,
-        perimeter: 4,
-      },
-      1,
-    );
-  });
-  it('2 pixels ROI', () => {
-    const mask = testUtils.createMask([
-      [0, 0, 0],
-      [0, 1, 1],
-      [0, 0, 0],
-    ]);
+  const convexHull = getConvexHull(mask);
+  expect(convexHull).toBeDeepCloseTo(
+    {
+      points: [
+        { column: 2, row: 1 },
+        { column: 2, row: 2 },
+        { column: 3, row: 2 },
+        { column: 3, row: 1 },
+      ],
+      surface: 1,
+      perimeter: 4,
+    },
+    1,
+  );
+});
+test('2 pixels ROI', () => {
+  const mask = testUtils.createMask([
+    [0, 0, 0],
+    [0, 1, 1],
+    [0, 0, 0],
+  ]);
 
-    const convexHull = getConvexHull(mask);
-    expect(convexHull).toBeDeepCloseTo(
-      {
-        points: [
-          { column: 1, row: 1 },
-          { column: 1, row: 2 },
-          { column: 3, row: 2 },
-          { column: 3, row: 1 },
-        ],
-        surface: 2,
-        perimeter: 6,
-      },
-      1,
-    );
-  });
-  it('5x5 cross', () => {
-    const mask = testUtils.createMask([
-      [0, 0, 1, 0, 0],
-      [0, 0, 1, 0, 0],
-      [1, 1, 1, 1, 1],
-      [0, 0, 1, 0, 0],
-      [0, 0, 1, 0, 0],
-    ]);
+  const convexHull = getConvexHull(mask);
+  expect(convexHull).toBeDeepCloseTo(
+    {
+      points: [
+        { column: 1, row: 1 },
+        { column: 1, row: 2 },
+        { column: 3, row: 2 },
+        { column: 3, row: 1 },
+      ],
+      surface: 2,
+      perimeter: 6,
+    },
+    1,
+  );
+});
+test('5x5 cross', () => {
+  const mask = testUtils.createMask([
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [1, 1, 1, 1, 1],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+  ]);
 
-    const convexHull = getConvexHull(mask);
-    expect(convexHull).toBeDeepCloseTo(
-      {
-        points: [
-          { column: 0, row: 2 },
-          { column: 0, row: 3 },
-          { column: 2, row: 5 },
-          { column: 3, row: 5 },
-          { column: 5, row: 3 },
-          { column: 5, row: 2 },
-          { column: 3, row: 0 },
-          { column: 2, row: 0 },
-        ],
-        surface: 17,
-        perimeter: 15.3,
-      },
-      1,
-    );
-  });
-  it('random shape', () => {
-    const mask = testUtils.createMask([
-      [0, 0, 1, 0, 0],
-      [0, 0, 1, 0, 0],
-      [0, 0, 1, 1, 1],
-      [0, 1, 0, 0, 0],
-      [1, 0, 0, 0, 0],
-    ]);
+  const convexHull = getConvexHull(mask);
+  expect(convexHull).toBeDeepCloseTo(
+    {
+      points: [
+        { column: 0, row: 2 },
+        { column: 0, row: 3 },
+        { column: 2, row: 5 },
+        { column: 3, row: 5 },
+        { column: 5, row: 3 },
+        { column: 5, row: 2 },
+        { column: 3, row: 0 },
+        { column: 2, row: 0 },
+      ],
+      surface: 17,
+      perimeter: 15.3,
+    },
+    1,
+  );
+});
+test('random shape', () => {
+  const mask = testUtils.createMask([
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 1, 1],
+    [0, 1, 0, 0, 0],
+    [1, 0, 0, 0, 0],
+  ]);
 
-    const convexHull = getConvexHull(mask);
-    expect(convexHull.points).toStrictEqual([
-      { column: 0, row: 4 },
-      { column: 0, row: 5 },
-      { column: 1, row: 5 },
-      { column: 5, row: 3 },
-      { column: 5, row: 2 },
-      { column: 3, row: 0 },
-      { column: 2, row: 0 },
-    ]);
-  });
-  it('empty mask', () => {
-    const mask = testUtils.createMask([
-      [0, 0],
-      [0, 0],
-    ]);
+  const convexHull = getConvexHull(mask);
+  expect(convexHull.points).toStrictEqual([
+    { column: 0, row: 4 },
+    { column: 0, row: 5 },
+    { column: 1, row: 5 },
+    { column: 5, row: 3 },
+    { column: 5, row: 2 },
+    { column: 3, row: 0 },
+    { column: 2, row: 0 },
+  ]);
+});
+test('empty mask', () => {
+  const mask = testUtils.createMask([
+    [0, 0],
+    [0, 0],
+  ]);
 
-    const result = mask.getConvexHull();
+  const result = mask.getConvexHull();
 
-    expect(result).toStrictEqual({
-      points: [],
-      surface: 0,
-      perimeter: 0,
-    });
+  expect(result).toStrictEqual({
+    points: [],
+    surface: 0,
+    perimeter: 0,
   });
 });
