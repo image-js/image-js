a0ddf46e5925dd6f3aed8b6fbb2fc55ad1489725
Fa-b <41373206+Fa-b@users.noreply.github.com>
2022-10-11 12:36:27 +0000

    added tests to improve coverage added niche fix for cases where binary image init data contains set bits out of boundaries

--- src/image/transform/__tests__/pad.js
+++ src/image/transform/__tests__/pad.js
@@ -1,6 +1,45 @@
+import binary from 'test/binary';
 import { Image } from 'test/common';
 
 describe('check the pad transform', function () {
+  it('check the right pad extract for a BINARY image', function () {
+    let image = new Image(5, 5, [0x03, 0x9c, 0xe0, 0x00], { kind: 'BINARY' });
+
+    let expected = binary`
+    00000
+    01110
+    01110
+    01110
+    00000
+  `;
+
+    expect(image.pad().data).toStrictEqual(expected.data);
+
+    expected = binary`
+    0000000
+    0000000
+    0011100
+    0011100
+    0011100
+    0000000
+    0000000
+  `;
+    expect(image.pad({ size: 1 }).data).toStrictEqual(expected.data);
+
+    expected = binary`
+    0000000
+    0000000
+    0000000
+    0011100
+    0011100
+    0011100
+    0000000
+    0000000
+    0000000
+  `;
+    expect(image.pad({ size: [1, 2] }).data).toStrictEqual(expected.data);
+  });
+
   it('check the right pad extract for GREY image', function () {
     let image = new Image(2, 2, [1, 2, 3, 4], { kind: 'GREY' });
 
@@ -9,6 +48,10 @@ describe('check the pad transform', function () {
     expect(Array.from(image.pad({ size: 1 }).data)).toStrictEqual([
       1, 1, 2, 2, 1, 1, 2, 2, 3, 3, 4, 4, 3, 3, 4, 4,
     ]);
+
+    expect(
+      Array.from(image.pad({ size: 1, algorithm: 'set', color: 0 }).data),
+    ).toStrictEqual([0, 0, 0, 0, 0, 1, 2, 0, 0, 3, 4, 0, 0, 0, 0, 0]);
 
     expect(Array.from(image.pad({ size: 2 }).data)).toStrictEqual([
       1, 1, 1, 2, 2, 2, 1, 1, 1, 2, 2, 2, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 3,
