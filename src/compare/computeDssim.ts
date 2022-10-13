7a97050425f377b049d14b8956060e41d70e0086
Oceane Patiny <oceane@patiny.com>
2022-10-13 11:18:02 +0200

    feat: implement computeDssim
    Still need to write the test.

---
+++ src/compare/computeDssim.ts
@@ -0,0 +1,21 @@
+import { Image } from '..';
+
+import { computeSsim, SsimOptions } from './computeSsim';
+
+/**
+ * Compute the Structural Dissimilarity (DSSIM) of two GREY images.
+ * https://en.wikipedia.org/wiki/Structural_similarity
+ *
+ * @param image - First image.
+ * @param otherImage - Second image.
+ * @param options
+ * @returns SSIM of the two images.
+ */
+export function computeDssim(
+  image: Image,
+  otherImage: Image,
+  options: SsimOptions = {},
+): number {
+  const ssim = computeSsim(image, otherImage, options);
+  return (1 - ssim) / 2;
+}
