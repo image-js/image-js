0c06b062f9231f82e95b674b9016b96fda3c1b3b
Oceane Patiny <oceane@patiny.com>
2022-10-18 16:14:06 +0200

    docs: fix computeSsim demo

--- demo/components/testFunctions/testComputeSsim.ts
+++ demo/components/testFunctions/testComputeSsim.ts
@@ -1,5 +1,5 @@
 import { Image, ImageColorModel } from '../../../src';
-import { computeSsim } from '../../../src/compare';
+import { computeSsim } from '../../../src/compare/computeSsim';
 
 /**
  * Compute the structural similarity of the input image and the image blurred.
@@ -9,20 +9,21 @@ import { computeSsim } from '../../../src/compare';
  */
 export function testComputeSsim(image: Image): Image {
   // It is the gaussian blur that is slow.
-  const blurry = image.gaussianBlur({ sigma: 5 });
-  const ssim = computeSsim(image, blurry, { windowSize: 100 });
+  const blurry = image.gaussianBlur({ sigma: 3 });
+  const ssim = computeSsim(image, blurry, {
+    windowSize: 100,
+    algorithm: 'fast',
+  });
 
-  console.log(ssim.ssimMap.data);
   let data = new Uint8Array(ssim.ssimMap.data);
-  console.log({ data });
   for (let i = 0; i < data.length; i++) {
     data[i] = ssim.ssimMap.data[i] * 255;
   }
 
-  console.log(data);
   const ssimMap = new Image(ssim.ssimMap.width, ssim.ssimMap.height, {
     colorModel: ImageColorModel.GREY,
     data,
   });
+
   return ssimMap;
 }
