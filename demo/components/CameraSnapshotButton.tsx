b8c196d6827bfdbf3317333a694b335217c19935
Oceane Patiny <oceane@patiny.com>
2022-10-27 16:28:22 +0200

    feat: implement CameraSnapshotButton

--- demo/components/CameraSnapshotButton.tsx
+++ demo/components/CameraSnapshotButton.tsx
@@ -1,11 +1,31 @@
-export default function CameraSnapshotButton() {
+import { MutableRefObject, RefObject } from 'react';
+
+import { readCanvas, Image } from '../../src';
+
+interface CameraSnapshotButtonProps {
+  snapshotImageRef: MutableRefObject<Image | null>;
+  setSnapshotUrl: (snapshotUrl: string) => void;
+  canvasInputRef: RefObject<HTMLCanvasElement>;
+}
+
+export default function CameraSnapshotButton(props: CameraSnapshotButtonProps) {
+  const { setSnapshotUrl, snapshotImageRef, canvasInputRef } = props;
+  function handleClick() {
+    if (canvasInputRef.current) {
+      const url = canvasInputRef.current.toDataURL();
+      const image = readCanvas(canvasInputRef.current);
+      setSnapshotUrl(url);
+      snapshotImageRef.current = image;
+    }
+  }
   return (
     <div>
       <button
         className="border p-1 rounded hover:bg-green-300 hover:font-bold"
         type="button"
+        onClick={handleClick}
       >
-        Take snaphshot
+        Take snapshot
       </button>
     </div>
   );
