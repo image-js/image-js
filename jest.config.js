c008828d4bc6db99036aa15f182149bb9b4f12d7
Michaël Zasso <targos@protonmail.com>
2022-11-29 13:56:44 +0100

    chore: update dependencies and remove obsolete image snapshots

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,8 @@ module.exports = {
   collectCoverage: true,
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testMatch: ['**/?(*.)+(test).[jt]s?(x)'],
+  reporters: [
+    "default",
+    "jest-image-snapshot/src/outdated-snapshot-reporter.js"
+  ]
 };
