237382b40d8572d13fd20dc84e995676e404eee4
Oceane Patiny <oceane@patiny.com>
2022-12-06 17:28:13 +0100

    fix(getBriefDescriptors): extract keypoint patch

--- src/featureMatching/getBriefDescriptors.ts
+++ src/featureMatching/getBriefDescriptors.ts
@@ -1,5 +1,7 @@
 import { Image } from '../Image';
 import { GaussianBlurOptions } from '../filters';
+import { Point } from '../geometry';
+import { InterpolationType } from '../utils/interpolatePixel';
 
 import { OrientedFastKeypoint } from './getOrientedFastKeypoints';
 
@@ -38,8 +40,46 @@ export function getBriefDescriptors(
   const smoothed = image.gaussianBlur(smoothingOptions);
 
   for (let keypoint of keypoints) {
-    let rotatedImage = image.rotate(keypoint.angle, {
-      origin: keypoint.origin,
+    const currentRow = keypoint.origin.row;
+    const currentColumn = keypoint.origin.column;
+    // crop smallest square surrounding the tilted patch of the keypoint
+    const cropWidth =
+      patchSize * (Math.cos(keypoint.angle) + Math.sin(keypoint.angle));
+    const cropped = extractSquareImage(smoothed, keypoint.origin, cropWidth);
+
+    const rotated = cropped.rotate(keypoint.angle, {
+      center: [currentColumn, currentRow],
+      interpolationType: InterpolationType.NEAREST,
     });
+    const patch = extractSquareImage(
+      rotated,
+      // I need this function a lot here -> refactor it??
+      image.getCoordinates('CENTER'),
+      patchSize,
+    );
   }
+}
+/**
+ * Crop the source image to given dimensions around the origin.
+ *
+ * @param image - Source image.
+ * @param origin - Center point for the crop.
+ * @param patchSize - Size of the returned image.
+ * @returns The square image around the origin extracted from the source image.
+ */
+function extractSquareImage(
+  image: Image,
+  origin: Point,
+  patchSize: number,
+): Image {
+  const cropOffset = (patchSize - 1) / 2;
+  const cropOrigin = {
+    column: origin.column - cropOffset,
+    row: origin.row - cropOffset,
+  };
+  return image.crop({
+    origin: cropOrigin,
+    width: patchSize,
+    height: patchSize,
+  });
 }
