9c7fc178da2beca0377a3f4573fe8c187e716290
Oceane Patiny <oceane@patiny.com>
2022-12-13 14:49:02 +0100

    feat: implement extractSquareImage

---
+++ src/featureMatching/utils/__tests__/extractSquareImage.test.ts
@@ -0,0 +1,40 @@
+import { extractSquareImage } from '../extractSquareImage';
+
+test('7x7 image, first origin', () => {
+  const image = testUtils.createGreyImage([
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [255, 255, 255, 0, 0, 0, 0],
+    [0, 0, 0, 255, 0, 0, 0],
+    [0, 0, 0, 255, 0, 0, 0],
+    [0, 0, 0, 255, 0, 0, 0],
+  ]);
+
+  const result = extractSquareImage(image, { column: 2, row: 2 }, 3);
+
+  expect(result).toMatchImageData([
+    [0, 0, 0],
+    [0, 0, 0],
+    [255, 255, 0],
+  ]);
+});
+
+test('7x7 image, second origin', () => {
+  const image = testUtils.createGreyImage([
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [255, 255, 255, 255, 0, 0, 0],
+    [0, 0, 0, 255, 0, 0, 0],
+    [0, 0, 0, 255, 0, 0, 0],
+    [0, 0, 0, 255, 0, 0, 0],
+  ]);
+
+  const result = extractSquareImage(image, { column: 3, row: 4 }, 3);
+  expect(result).toMatchImageData([
+    [255, 255, 0],
+    [0, 255, 0],
+    [0, 255, 0],
+  ]);
+});
---
+++ src/featureMatching/utils/extractSquareImage.ts
@@ -0,0 +1,28 @@
+import { Image } from '../../Image';
+import { Point } from '../../geometry';
+
+/**
+ * Crop the source image to given dimensions around the origin.
+ *
+ * @param image - Source image.
+ * @param origin - Center point for the crop.
+ * @param patchSize - Size of the returned image.
+ * @returns The square image around the origin extracted from the source image.
+ */
+export function extractSquareImage(
+  image: Image,
+  origin: Point,
+  patchSize: number,
+): Image {
+  const cropOffset = (patchSize - 1) / 2;
+  const cropOrigin = {
+    column: origin.column - cropOffset,
+    row: origin.row - cropOffset,
+  };
+
+  return image.crop({
+    origin: cropOrigin,
+    width: patchSize,
+    height: patchSize,
+  });
+}
