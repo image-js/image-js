920d719e1dcd37f5ea67da9fdf82f83a8ba0274c
Oceane Patiny <oceane@patiny.com>
2022-12-29 16:26:36 +0100

    feat: implement getKeypointColor
    Refs: #188

---
+++ src/featureMatching/utils/getKeypointColor.ts
@@ -0,0 +1,28 @@
+import { FastKeypoint } from '../getFastKeypoints';
+
+/**
+ * Get the shade the keypoint with given index should have (the color is an indicator
+ * of the score). The keypoints should be sorted with highest score first.
+ *
+ * @param keypoints - The sorted keypoints.
+ * @param index - Index of the keypoint.
+ * @param colors - The colors from which to pick (sorted from brightest to darkest).
+ * @returns The color the keypoint should have.
+ */
+export function getKeypointColor(
+  keypoints: FastKeypoint[],
+  index: number,
+  colors: number[][],
+): number[] {
+  const maxScore = keypoints[0].score;
+  const minScore = keypoints[keypoints.length - 1].score;
+  if (minScore === maxScore) {
+    return colors[0];
+  }
+  const score = keypoints[index].score;
+  const colorIndex =
+    colors.length -
+    Math.floor(((colors.length - 1) * score) / (maxScore - minScore));
+
+  return colors[colorIndex];
+}
