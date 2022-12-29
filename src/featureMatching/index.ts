05aa75a2acb3ee9083f2d98b29d58016804beb5a
Oceane Patiny <oceane@patiny.com>
2022-12-29 09:23:17 +0100

    fix: update featureMatching index

--- src/featureMatching/index.ts
+++ src/featureMatching/index.ts
@@ -1,3 +1,6 @@
+export * from './bruteForceMatch';
+export * from './drawMatches';
+export * from './getBriefDescriptors';
 export * from './getFastKeypoints';
 export * from './getIntensityCentroid';
 export * from './getOrientedFastKeypoints';
