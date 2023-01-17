1dc4bfca6cb20f8da052d5c97107cfdeb00fb5c9
Oceane Patiny <oceane@patiny.com>
2023-01-17 13:59:20 +0100

    feat: implement getBasicMontage

---
+++ src/featureMatching/visualize/__tests__/getBasicMontage.test.ts
@@ -0,0 +1,8 @@
+import { getBasicMontage } from './getBasicMontage';
+
+test('check montage is correct', () => {
+  const image1 = testUtils.load('featureMatching/alphabet.jpg');
+  const image2 = testUtils.load('featureMatching/alphabetRotated-10.jpg');
+
+  expect(getBasicMontage(image1, image2)).toMatchImageSnapshot();
+});
---
+++ src/featureMatching/visualize/__tests__/getBasicMontage.ts
@@ -0,0 +1,30 @@
+import { Image, ImageColorModel } from '../../../Image';
+
+/**
+ * Create a new image containing the input images side by side.
+ *
+ * @param image1 - First image.
+ * @param image2 - Second image.
+ * @returns The basic montage.
+ */
+export function getBasicMontage(image1: Image, image2: Image): Image {
+  if (image1.colorModel !== ImageColorModel.RGB) {
+    image1 = image1.convertColor(ImageColorModel.RGB);
+  }
+  if (image2.colorModel !== ImageColorModel.RGB) {
+    image2 = image2.convertColor(ImageColorModel.RGB);
+  }
+
+  const result = new Image(
+    image1.width + image2.width,
+    Math.max(image1.height, image2.height),
+  );
+
+  image1.copyTo(result, { out: result });
+  image2.copyTo(result, {
+    out: result,
+    origin: { column: image1.width, row: 0 },
+  });
+
+  return result;
+}
