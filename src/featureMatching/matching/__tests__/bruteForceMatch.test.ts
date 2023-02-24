6de32468d7dbfa8fdb7e2a67210715836f3a4639
Oceane Patiny <oceane@patiny.com>
2023-02-24 17:11:32 +0100

    test: scalene triangle test

--- src/featureMatching/matching/__tests__/bruteForceMatch.test.ts
+++ src/featureMatching/matching/__tests__/bruteForceMatch.test.ts
@@ -4,6 +4,7 @@ import {
   getBriefDescriptors,
 } from '../../descriptors/getBriefDescriptors';
 import { getOrientedFastKeypoints } from '../../keypoints/getOrientedFastKeypoints';
+import { Montage } from '../../visualize/Montage';
 import { bruteForceOneMatch } from '../bruteForceMatch';
 
 /**
@@ -14,8 +15,8 @@ import { bruteForceOneMatch } from '../bruteForceMatch';
  */
 function getDescriptors(image: Image): BriefDescriptor[] {
   const grey = image.convertColor(ImageColorModel.GREY);
-  const sourceKeypoints = getOrientedFastKeypoints(grey);
-  return getBriefDescriptors(grey, sourceKeypoints).descriptors;
+  const keypoints = getOrientedFastKeypoints(grey);
+  return getBriefDescriptors(grey, keypoints).descriptors;
 }
 
 test('nbBestMatches = 5', () => {
@@ -31,4 +32,36 @@ test('nbBestMatches = 5', () => {
   );
 
   expect(matches.length).toBe(5);
+});
+
+test.only('scalene triangle', () => {
+  const source = testUtils
+    .load('featureMatching/scaleneTriangle.png')
+    .convertColor(ImageColorModel.GREY);
+  const sourceKeypoints = getOrientedFastKeypoints(source);
+  const sourceDescriptors = getBriefDescriptors(
+    source,
+    sourceKeypoints,
+  ).descriptors;
+  const destination = testUtils
+    .load('featureMatching/scaleneTriangle2.png')
+    .convertColor(ImageColorModel.GREY);
+  const destinationKeypoints = getOrientedFastKeypoints(destination);
+  const destinationDescriptors = getBriefDescriptors(
+    destination,
+    destinationKeypoints,
+  ).descriptors;
+
+  const matches = bruteForceOneMatch(sourceDescriptors, destinationDescriptors);
+
+  // expect(matches.length).toBe(2);
+
+  const montage = new Montage(source, destination);
+  montage.drawKeypoints(sourceKeypoints);
+  montage.drawKeypoints(destinationKeypoints, {
+    origin: montage.destinationOrigin,
+  });
+  montage.drawMatches(matches, sourceKeypoints, destinationKeypoints);
+
+  expect(montage.image).toMatchImageSnapshot();
 });
