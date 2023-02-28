d5678263c0f2322a7babd751e70c9b9c43ee1d7f
Daniel Kostro <kostro.d@gmail.com>
2023-02-28 11:36:10 +0100

    chore: implement browser versions for node-only api methods

---
+++ src/load/read.browser.ts
@@ -0,0 +1,13 @@
+/**
+ * read
+ */
+export function read() {
+  throw new Error('read is not implemented in the browser');
+}
+
+/**
+ * readSync
+ */
+export function readSync() {
+  throw new Error('readSync is not implemented in the browser');
+}
---
+++ src/save/write.browser.ts
@@ -0,0 +1,13 @@
+/**
+ * write
+ */
+export function write() {
+  throw new Error('write is not implemented in the browser');
+}
+
+/**
+ * writeSync
+ */
+export function writeSync() {
+  throw new Error('writeSync is not implemented in the browser');
+}
