35580f52e2aa9fb67a3a383e216ca0a624706838
Oceane Patiny <oceane@patiny.com>
2023-03-10 10:05:51 +0100

    test: add rotated images of scalene triangle

--- scripts/generateFMtestImages.ts
+++ scripts/generateFMtestImages.ts
@@ -1,26 +1,33 @@
 // generate some variations of the alphabet image for feature matching
 // run this script with: ts-node --log-error generateFMtestImages.ts
 import { ImageColorModel, readSync, writeSync, Image } from '../src';
+import { join } from 'path';
 
-const original = readSync('../test/img/featureMatching/alphabet.jpg');
+console.log(__dirname);
 
-const angles = [-10, -5, -2, 2, 5, 10];
+const basePath = join(__dirname, '../test/img/featureMatching/polygons');
+
+console.log(basePath);
+
+const original = readSync(`${basePath}/scaleneTriangle.png`);
+
+const angles = [10, 90, 180];
 for (let angle of angles) {
   const rotated = original.transformRotate(angle, { fullImage: true });
-  writeSync(`../test/img/featureMatching/alphabetRotated${angle}.jpg`, rotated);
+  writeSync(`${basePath}/scaleneTriangle${angle}.png`, rotated);
 }
 
-const translations = [10, 20, 50];
-const empty = new Image(original.width + 100, original.height + 100, {
-  colorModel: ImageColorModel.RGBA,
-}).fill(100);
+// const translations = [10, 20, 50];
+// const empty = new Image(original.width + 100, original.height + 100, {
+//   colorModel: ImageColorModel.RGBA,
+// }).fill(100);
 
-for (let translation of translations) {
-  const translated = original.copyTo(empty, {
-    origin: { column: 50, row: translation },
-  });
-  writeSync(
-    `../test/img/featureMatching/alphabetTranslated${translation}.jpg`,
-    translated,
-  );
-}
+// for (let translation of translations) {
+//   const translated = original.copyTo(empty, {
+//     origin: { column: 50, row: translation },
+//   });
+//   writeSync(
+//     `../test/img/featureMatching/alphabetTranslated${translation}.jpg`,
+//     translated,
+//   );
+// }
