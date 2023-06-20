badd71a9f29d7c11b63a6b5d31d89ecccf486649
Oceane Patiny <oceane@patiny.com>
2023-06-20 14:21:44 +0200

    feat: implement filterEuclidianDistance

---
+++ src/featureMatching/matching/__tests__/filterEuclidianDistance.test.ts
@@ -0,0 +1,33 @@
+import { Match } from '../bruteForceMatch';
+import { filterEuclidianDistance } from '../filterEuclidianDistancetrue';
+
+test('3 matches', () => {
+  const matches: Match[] = [
+    { sourceIndex: 0, destinationIndex: 0, distance: 1 },
+    { sourceIndex: 1, destinationIndex: 2, distance: 3 },
+    { sourceIndex: 2, destinationIndex: 0, distance: 5 },
+  ];
+
+  const sourceKeypoints = [
+    { origin: { column: 0, row: 0 }, score: 0 },
+    { origin: { column: 1, row: 3 }, score: 0 },
+    { origin: { column: 5, row: 6 }, score: 0 },
+  ];
+
+  const destinationKeypoints = [
+    { origin: { column: 0, row: 0 }, score: 0 },
+    { origin: { column: 0, row: 0 }, score: 0 },
+    { origin: { column: 0, row: 0 }, score: 0 },
+  ];
+
+  const result = filterEuclidianDistance(
+    matches,
+    sourceKeypoints,
+    destinationKeypoints,
+  );
+
+  expect(result).toStrictEqual([
+    { sourceIndex: 0, destinationIndex: 0, distance: 1 },
+    { sourceIndex: 1, destinationIndex: 2, distance: 3 },
+  ]);
+});
---
+++ src/featureMatching/matching/filterEuclidianDistancetrue.ts
@@ -0,0 +1,67 @@
+import { FastKeypoint, Match } from '..';
+import { Point } from '../..';
+
+export interface FilterEuclidianDistanceMatchesOptions {
+  /**
+   * The origin of the destination image relative to the top-left corner of the source image.
+   * @default { column: 0, row: 0 }
+   */
+  origin?: Point;
+}
+
+/**
+ * Use this function to only keep the match from source to destination with
+ * the smallest distance (score) for each destination keypoint.
+ * @param matches - Matches from source to destination.
+ * @param sourceKeypoints - Source keypoints.
+ * @param destinationKeypoints - Destination keypoints.
+ * @param options - Filter smallest distance matches options.
+ * @returns Only the matches from source to destination with the smallest distance.
+ */
+export function filterEuclidianDistance(
+  matches: Match[],
+  sourceKeypoints: FastKeypoint[],
+  destinationKeypoints: FastKeypoint[],
+  options: FilterEuclidianDistanceMatchesOptions = {},
+): Match[] {
+  const { origin = { column: 0, row: 0 } } = options;
+
+  const sorted = matches.sort(
+    (a, b) => a.destinationIndex - b.destinationIndex,
+  );
+
+  const result: Match[] = [];
+  let sameDestMatches: Match[] = [];
+  let currentIndex = sorted[0].destinationIndex;
+  for (const match of sorted) {
+    if (match.destinationIndex === currentIndex) {
+      sameDestMatches.push(match);
+    } else {
+      result.push(
+        ...sameDestMatches
+          .sort((a, b) => distanceSquared(a) - distanceSquared(b))
+          .slice(0, 1),
+      );
+
+      currentIndex = match.destinationIndex;
+      sameDestMatches = [match];
+    }
+  }
+  result.push(
+    ...sameDestMatches.sort((a, b) => a.distance - b.distance).slice(0, 1),
+  );
+  return result;
+
+  function distanceSquared(match: Match): number {
+    return (
+      (sourceKeypoints[match.sourceIndex].origin.row -
+        (destinationKeypoints[match.destinationIndex].origin.row -
+          origin.row)) **
+        2 +
+      (sourceKeypoints[match.sourceIndex].origin.column -
+        destinationKeypoints[match.destinationIndex].origin.column -
+        origin.column) **
+        2
+    );
+  }
+}
