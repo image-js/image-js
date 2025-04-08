e42dcf703d341d8523120931d6ae4ab591bbeff6
Yacine Laghdiri <yacine.laghdiri@gmail.com>
2025-04-08 15:52:22 +0200

    fix: default value definition fix

--- src/image/operator/cannyEdge.js
+++ src/image/operator/cannyEdge.js
@@ -4,9 +4,9 @@ import cannyEdgeDetector from 'canny-edge-detector';
  * @memberof Image
  * @instance
  * @param {object} [options]
- * @param {number} [options.gaussianBlur] - Sigma parameter for the gaussian filter step (default: 1.1)
- * @param {number} [options.lowThreshold] - Low threshold for the hysteresis procedure (default: 10)
- * @param {number} [options.highThreshold] - High threshold for the hysteresis procedure (default: 30)
+ * @param {number} [options.gaussianBlur=1.1] - Sigma parameter for the gaussian filter step
+ * @param {number} [options.lowThreshold=10] - Low threshold for the hysteresis procedure
+ * @param {number} [options.highThreshold=30] - High threshold for the hysteresis procedure
  * @return {Image}
  */
 export default function cannyEdge(options) {
