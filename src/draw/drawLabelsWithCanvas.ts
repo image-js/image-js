644b823e09336de57d51720957b3a83b0b84fbbd
EscapedGibbon <maximgolubev2@gmail.com>
2025-08-14 09:21:35 +0200

    feat: add functionality to drawLabelsWithCanvas

--- src/draw/drawLabelsWithCanvas.ts
+++ src/draw/drawLabelsWithCanvas.ts
@@ -1,31 +1,33 @@
-import type { Image } from '../Image.js';
 import { Canvas } from 'skia-canvas';
+
+import type { Image } from '../Image.js';
+import type { Point } from '../geometry/index.js';
 import { readCanvas } from '../load/readCanvas.js';
-import { writeCanvas } from '../save/writeCanvas.js';
-import { readSync } from '../load/read.js';
-import { writeSync } from '../save/write.js';
 
-export function drawLabelWithCanvas(image: Image, label: string) {
+interface DrawLabelsWithCanvasOptions {
+  font?: string;
+  fontColor?: number[];
+}
+
+export function drawLabelWithCanvas(
+  image: Image,
+  labels: string[],
+  coordinates: Point[],
+  options: DrawLabelsWithCanvasOptions = {},
+) {
   const canvas = new Canvas(image.width, image.height);
-
+  const { font = '16px sans-serif', fontColor = [255, 255, 255] } = options;
   const ctx = canvas.getContext('2d');
   const imageData = ctx.createImageData(image.width, image.height);
   imageData.data.set(image.getRawImage().data);
   ctx.putImageData(imageData, 0, 0);
-  ctx.fillText(label, 10, 10);
+  ctx.font = font;
+  for (let i = 0; i < labels.length; i++) {
+    const coordinate = coordinates[i % coordinates.length];
+    ctx.fillStyle = `rgba(${fontColor.join(',')})`;
+    ctx.fillText(labels[i % labels.length], coordinate.column, coordinate.row);
+  }
+
   image = readCanvas(canvas);
   return image;
 }
-
-let image = readSync(
-  '/Users/maxim/git/zakodium/image-js/test/img/various/screws.png',
-);
-image.gaussianBlur({ sigma: 0, sizeX: 2 });
-
-/*
-image = drawLabelWithCanvas(
-  image,
-  'This is a very long text that i want to write just to check if the function works.',
-);
-*/
-writeSync('./testingLabels.png', image);
