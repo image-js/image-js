224c3a2e2993a504ebc3ca8ed0dc91aa2cbb9c3c
EscapedGibbon <maximgolubev2@gmail.com>
2025-08-18 18:10:01 +0200

    fix: fix and improve drawLabel performance

---
+++ src/draw/drawLabels.ts
@@ -0,0 +1,140 @@
+import { Canvas } from 'skia-canvas';
+
+import { Image } from '../Image.ts';
+import type { Point } from '../geometry/index.ts';
+
+interface DrawLabelsWithCanvasOptions {
+  /**
+   *  Size and type of font.
+   */
+  font?: string;
+  /**
+   *  Font color.
+   */
+  fontColor?: number[];
+}
+/**
+ * Draws different labels on images.
+ * @param image - Image to draw labels on.
+ * @param labels - Labels to draw.
+ * @param coordinates - Coordinates where to draw labels.
+ * @param options - DrawLabelsWithCanvasOptions.
+ * @returns RGBA image.
+ */
+export function drawLabelWithCanvas(
+  image: Image,
+  labels: string[],
+  coordinates: Point[],
+  options: DrawLabelsWithCanvasOptions = {},
+) {
+  const canvas = new Canvas(image.width, image.height);
+  const { font = '12px Roboto', fontColor = [255, 255, 255] } = options;
+  const ctx = canvas.getContext('2d');
+  const newData = toRgba8(image);
+  const imageData = ctx.createImageData(image.width, image.height);
+  imageData.data.set(newData);
+  ctx.putImageData(imageData, 0, 0);
+  ctx.font = font;
+  for (let i = 0; i < labels.length; i++) {
+    const coordinate = coordinates[i % coordinates.length];
+    ctx.fillStyle = `rgba(${fontColor.join(',')})`;
+    ctx.fillText(labels[i % labels.length], coordinate.column, coordinate.row);
+  }
+
+  const resultData = fromRgba8(
+    ctx.getImageData(0, 0, image.width, image.height).data,
+    image.channels,
+  );
+  const newImage = new Image(image.width, image.height, {
+    data: resultData,
+    colorModel: image.colorModel,
+  });
+
+  return newImage;
+}
+/**
+ * Converts image data to RGBA8 format to be compatible with canvas data format.
+ * @param image - Image to convert data from.
+ * @returns Clamped array of RGBA8 data.
+ */
+function toRgba8(image: Image) {
+  const result = new Uint8ClampedArray(image.width * image.height * 4);
+  const numberOfChannels = image.channels;
+  const bitDepth = image.bitDepth;
+  const srcData = image.getRawImage().data;
+  let index = 0;
+  switch (numberOfChannels) {
+    case 4:
+      for (let i = 0; i < srcData.length; i += numberOfChannels) {
+        result[index++] = srcData[i] >>> (bitDepth - 8);
+      }
+      return result;
+    case 3:
+      for (let i = 0; i < srcData.length; i += numberOfChannels) {
+        result[index++] = srcData[i] >>> (bitDepth - 8);
+        result[index++] = srcData[i + 1] >>> (bitDepth - 8);
+        result[index++] = srcData[i + 2] >>> (bitDepth - 8);
+        result[index++] = 255 >>> (bitDepth - 8);
+      }
+      return result;
+    case 2:
+      for (let i = 0; i < srcData.length; i += numberOfChannels) {
+        result[index++] = srcData[i] >>> (bitDepth - 8);
+        result[index++] = srcData[i] >>> (bitDepth - 8);
+        result[index++] = srcData[i] >>> (bitDepth - 8);
+        result[index++] = srcData[i + 1] >>> (bitDepth - 8);
+      }
+      return result;
+    case 1:
+      for (let i = 0; i < srcData.length; i += numberOfChannels) {
+        result[index++] = srcData[i] >>> (bitDepth - 8);
+        result[index++] = srcData[i] >>> (bitDepth - 8);
+        result[index++] = srcData[i] >>> (bitDepth - 8);
+        result[index++] = 255 >>> (bitDepth - 8);
+      }
+
+      return result;
+    default:
+      throw new RangeError('Unknown color model.');
+  }
+}
+/**
+ * Converts RGBA8 data to 8-bit data of image's inital color model.
+ * @param srcData - Source image data.
+ * @param numberOfChannels - Number of channels of the initial image.
+ * @returns Array with converted image data.
+ */
+function fromRgba8(srcData: Uint8ClampedArray, numberOfChannels: number) {
+  const rgbaPixelSize = 4;
+  const result = new Uint8ClampedArray(
+    (srcData.length / rgbaPixelSize) * numberOfChannels,
+  );
+  let index = 0;
+  switch (numberOfChannels) {
+    case 4:
+      for (let i = 0; i < srcData.length; i += rgbaPixelSize) {
+        result[index++] = srcData[i];
+      }
+      return result;
+    case 3:
+      for (let i = 0; i < srcData.length; i += rgbaPixelSize) {
+        result[index++] = srcData[i];
+        result[index++] = srcData[i + 1];
+        result[index++] = srcData[i + 2];
+      }
+      return result;
+    case 2:
+      for (let i = 0; i < srcData.length; i += rgbaPixelSize) {
+        result[index++] = srcData[i];
+        result[index++] = 255;
+      }
+      return result;
+    case 1:
+      for (let i = 0; i < srcData.length; i += rgbaPixelSize) {
+        result[index++] = srcData[i];
+      }
+      return result;
+    default:
+      throw new RangeError('Unknown color model.');
+  }
+}
