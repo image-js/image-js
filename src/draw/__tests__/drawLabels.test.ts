de51a82d9e8c69f9c3f4cb0b6271e89636db3dee
EscapedGibbon <maximgolubev2@gmail.com>
2025-08-22 12:44:01 +0200

    test: update failure threshold

--- src/draw/__tests__/drawLabels.test.ts
+++ src/draw/__tests__/drawLabels.test.ts
@@ -14,7 +14,7 @@ test('draw H on an image', () => {
   });
 
   expect(newImage).toMatchImageSnapshot({
-    failureThreshold: 0.5,
+    failureThreshold: 0.06,
     failureThresholdType: 'percent',
   });
 });
@@ -28,7 +28,7 @@ test('draw transparent H', () => {
   });
 
   expect(newImage).toMatchImageSnapshot({
-    failureThreshold: 0.5,
+    failureThreshold: 0.05,
     failureThresholdType: 'percent',
   });
 });
@@ -56,7 +56,7 @@ test('draw several labels', () => {
   });
 
   expect(newImage).toMatchImageSnapshot({
-    failureThreshold: 0.5,
+    failureThreshold: 0.05,
     failureThresholdType: 'percent',
   });
 });
@@ -95,7 +95,7 @@ test('test grey image', () => {
 
   expect(newImage.colorModel).toStrictEqual(image.colorModel);
   expect(newImage).toMatchImageSnapshot({
-    failureThreshold: 10,
+    failureThreshold: 0.05,
     failureThresholdType: 'percent',
   });
 });
@@ -112,7 +112,7 @@ test('test grey image with alpha', () => {
 
   expect(newImage.colorModel).toStrictEqual(image.colorModel);
   expect(newImage).toMatchImageSnapshot({
-    failureThreshold: 0.5,
+    failureThreshold: 0.15,
     failureThresholdType: 'percent',
   });
 });
@@ -129,7 +129,7 @@ test('test incomplete fill color', () => {
 
   expect(newImage.colorModel).toStrictEqual(image.colorModel);
   expect(newImage).toMatchImageSnapshot({
-    failureThreshold: 0.5,
+    failureThreshold: 0.05,
     failureThresholdType: 'percent',
   });
 });
