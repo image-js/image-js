25f50c6b6de880fd85ba17ef79067401e1f14093
EscapedGibbon <maximgolubev2@gmail.com>
2026-02-18 14:51:58 +0100

    fix: fix typo

--- src/featureMatching/keypoints/__tests__/getHarrisScore.test.ts
+++ src/featureMatching/keypoints/__tests__/getHarrisScore.test.ts
@@ -139,7 +139,7 @@ test('windowSize error', () => {
 
   const origin = { row: fastRadius, column: fastRadius };
 
-  expect(() => getHarrisScore(image, origin, { windowSize: 6 })).toThrowErrors(
+  expect(() => getHarrisScore(image, origin, { windowSize: 6 })).toThrow(
     'windowSize must be an odd integer',
   );
 });
