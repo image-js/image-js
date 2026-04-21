0806360f3ba04b59ecbf092111ec7fedde060b6d
EscapedGibbon <maximgolubev2@gmail.com>
2026-04-21 12:00:53 +0200

    test: add testing cases

--- src/maskAnalysis/__tests__/getBorderPoints.test.ts
+++ src/maskAnalysis/__tests__/getBorderPoints.test.ts
@@ -33,21 +33,6 @@ test('5x6 mask with hole, no inner borders', () => {
 
   const bordersMask = Mask.fromPoints(mask.width, mask.height, points);
 
-  expect(points).toStrictEqual([
-    { column: 0, row: 1 },
-    { column: 1, row: 1 },
-    { column: 2, row: 1 },
-    { column: 3, row: 1 },
-    { column: 4, row: 1 },
-    { column: 3, row: 2 },
-    { column: 3, row: 3 },
-    { column: 4, row: 4 },
-    { column: 3, row: 4 },
-    { column: 2, row: 4 },
-    { column: 1, row: 4 },
-    { column: 1, row: 3 },
-    { column: 1, row: 2 },
-  ]);
   expect(bordersMask).toMatchMaskData([
     [0, 0, 0, 0, 0],
     [1, 1, 1, 1, 1],
@@ -73,32 +58,6 @@ test('5x5 mask with hole, inner borders, allow corners', () => {
   });
   const bordersMask = Mask.fromPoints(mask.width, mask.height, points);
 
-  expect(points).toStrictEqual([
-    { column: 0, row: 0 },
-    { column: 1, row: 0 },
-    { column: 2, row: 0 },
-    { column: 3, row: 0 },
-    { column: 4, row: 0 },
-    { column: 4, row: 1 },
-    { column: 4, row: 2 },
-    { column: 4, row: 3 },
-    { column: 4, row: 4 },
-    { column: 3, row: 4 },
-    { column: 2, row: 4 },
-    { column: 1, row: 4 },
-    { column: 0, row: 4 },
-    { column: 0, row: 3 },
-    { column: 0, row: 2 },
-    { column: 0, row: 1 },
-    { column: 1, row: 1 },
-    { column: 2, row: 1 },
-    { column: 1, row: 2 },
-    { column: 1, row: 3 },
-    { column: 2, row: 3 },
-    { column: 3, row: 3 },
-    { column: 3, row: 2 },
-    { column: 3, row: 1 },
-  ]);
   expect(bordersMask).toMatchMask(mask);
 });
 
@@ -123,5 +82,4 @@ test('5x6 mask with hole, inner borders', () => {
     [1, 1, 0, 0, 1],
     [1, 1, 1, 1, 0],
   ]);
-  expect(points).toHaveLength(19);
 });
---
+++ src/maskAnalysis/__tests__/getExternalContour.test.ts
@@ -0,0 +1,207 @@
+import { expect, test } from 'vitest';
+
+import { Mask } from '../../Mask.ts';
+import { getExternalContour } from '../getExternalContour.js';
+
+test('simple test', () => {
+  const testMask = testUtils.createMask([
+    [0, 0, 0],
+    [0, 1, 1],
+    [0, 0, 0],
+  ]);
+  const points = getExternalContour(testMask);
+
+  expect(points).toStrictEqual([
+    { column: 1, row: 1 },
+    { column: 2, row: 1 },
+  ]);
+});
+
+test('simple test with Pavlidis', () => {
+  const testMask = testUtils.createMask([
+    [0, 0, 0],
+    [0, 1, 1],
+    [0, 0, 0],
+  ]);
+  const points = getExternalContour(testMask, { connectivity: 4 });
+
+  expect(points).toStrictEqual([
+    { column: 1, row: 1 },
+    { column: 2, row: 1 },
+  ]);
+});
+
+test('vertical line', () => {
+  const testMask = testUtils.createMask([
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 0],
+  ]);
+  const points = getExternalContour(testMask);
+
+  expect(points).toStrictEqual([
+    { column: 1, row: 0 },
+    { column: 1, row: 1 },
+    { column: 1, row: 2 },
+  ]);
+});
+
+test('vertical line with Pavlidis', () => {
+  const testMask = testUtils.createMask([
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 0],
+  ]);
+  const points = getExternalContour(testMask, { connectivity: 4 });
+
+  expect(points).toStrictEqual([
+    { column: 1, row: 0 },
+    { column: 1, row: 1 },
+    { column: 1, row: 2 },
+  ]);
+});
+
+test('square', () => {
+  const testMask = testUtils.createMask([
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+  ]);
+  const points = getExternalContour(testMask);
+
+  expect(points).toStrictEqual([
+    { column: 0, row: 0 },
+    { column: 1, row: 0 },
+    { column: 2, row: 0 },
+    { column: 2, row: 1 },
+    { column: 2, row: 2 },
+    { column: 1, row: 2 },
+    { column: 0, row: 2 },
+    { column: 0, row: 1 },
+  ]);
+});
+
+test('square with Pavlidis', () => {
+  const testMask = testUtils.createMask([
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+  ]);
+  const points = getExternalContour(testMask, { connectivity: 4 });
+
+  expect(points).toStrictEqual([
+    { column: 0, row: 0 },
+    { column: 1, row: 0 },
+    { column: 2, row: 0 },
+    { column: 2, row: 1 },
+    { column: 2, row: 2 },
+    { column: 1, row: 2 },
+    { column: 0, row: 2 },
+    { column: 0, row: 1 },
+  ]);
+});
+
+test('cross', () => {
+  const testMask = testUtils.createMask([
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 1, 0],
+  ]);
+  const points = getExternalContour(testMask);
+
+  expect(points).toStrictEqual([
+    { column: 1, row: 0 },
+    { column: 2, row: 1 },
+    { column: 1, row: 2 },
+    { column: 0, row: 1 },
+  ]);
+});
+
+test('1 pixel ROI', () => {
+  const mask = testUtils.createMask([
+    [0, 0, 0],
+    [0, 0, 1],
+    [0, 0, 0],
+  ]);
+
+  const convexHull = getExternalContour(mask);
+
+  expect(convexHull).toBeDeepCloseTo([{ column: 2, row: 1 }]);
+});
+
+test('random shape', () => {
+  const mask = testUtils.createMask([
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 1, 1],
+    [0, 1, 0, 0, 0],
+    [1, 0, 0, 0, 0],
+  ]);
+  const points = getExternalContour(mask);
+
+  expect(points).toBeDeepCloseTo([
+    { column: 2, row: 0 },
+    { column: 2, row: 1 },
+    { column: 3, row: 2 },
+    { column: 4, row: 2 },
+    { column: 2, row: 2 },
+    { column: 1, row: 3 },
+    { column: 0, row: 4 },
+  ]);
+});
+
+test('random shape with Pavlidis', () => {
+  const mask = testUtils.createMask([
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 1, 1],
+    [0, 1, 0, 0, 0],
+    [1, 0, 0, 0, 0],
+  ]);
+  const points = getExternalContour(mask);
+
+  expect(points).toBeDeepCloseTo([
+    { column: 2, row: 0 },
+    { column: 2, row: 1 },
+    { column: 3, row: 2 },
+    { column: 4, row: 2 },
+    { column: 2, row: 2 },
+    { column: 1, row: 3 },
+    { column: 0, row: 4 },
+  ]);
+});
+
+test('empty mask', () => {
+  const mask = testUtils.createMask([
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ]);
+
+  const contour = getExternalContour(mask);
+
+  expect(contour).toStrictEqual([]);
+});
+
+test('5x6 mask with hole, no inner borders', () => {
+  const mask = testUtils.createMask([
+    [0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1],
+    [0, 1, 0, 1, 0],
+    [0, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0],
+  ]);
+  const points = getExternalContour(mask);
+
+  const bordersMask = Mask.fromPoints(mask.width, mask.height, points);
+
+  expect(bordersMask).toMatchMaskData([
+    [0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1],
+    [0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0],
+    [0, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0],
+  ]);
+});
