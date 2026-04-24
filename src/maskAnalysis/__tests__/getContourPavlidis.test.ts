aa50549e34ed260ed62e95f8856cc66433e2f6dd
EscapedGibbon <maximgolubev2@gmail.com>
2026-04-24 14:20:09 +0200

    test: add more tests on concave shapes

--- src/maskAnalysis/__tests__/getContourPavlidis.test.ts
+++ src/maskAnalysis/__tests__/getContourPavlidis.test.ts
@@ -133,3 +133,41 @@ test('concave shape', () => {
     { column: 0, row: 2 },
   ]);
 });
+
+test('concave shape upside down', () => {
+  const testMask = testUtils.createMask([
+    [1, 0, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+  ]);
+  const points = getContourPavlidis(testMask);
+
+  expect(points).toStrictEqual([
+    { column: 0, row: 0 },
+    { column: 0, row: 1 },
+    { column: 0, row: 2 },
+    { column: 1, row: 2 },
+    { column: 2, row: 2 },
+    { column: 2, row: 1 },
+    { column: 2, row: 0 },
+  ]);
+});
+
+test('concave shape turned right', () => {
+  const testMask = testUtils.createMask([
+    [1, 1, 1],
+    [0, 0, 1],
+    [1, 1, 1],
+  ]);
+  const points = getContourPavlidis(testMask);
+
+  expect(points).toStrictEqual([
+    { column: 0, row: 0 },
+    { column: 1, row: 0 },
+    { column: 2, row: 0 },
+    { column: 2, row: 1 },
+    { column: 2, row: 2 },
+    { column: 1, row: 2 },
+    { column: 0, row: 2 },
+  ]);
+});
