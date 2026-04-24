654e07fb0640ff6022a9f6f511685a2ab9671190
EscapedGibbon <maximgolubev2@gmail.com>
2026-04-24 14:32:56 +0200

    test: fix moore test

--- src/maskAnalysis/__tests__/getContourMoore.test.ts
+++ src/maskAnalysis/__tests__/getContourMoore.test.ts
@@ -146,11 +146,11 @@ test('concave shape upside down', () => {
   expect(points).toStrictEqual([
     { column: 0, row: 0 },
     { column: 0, row: 1 },
-    { column: 0, row: 2 },
     { column: 1, row: 2 },
-    { column: 2, row: 2 },
     { column: 2, row: 1 },
     { column: 2, row: 0 },
+    { column: 2, row: 2 },
+    { column: 0, row: 2 },
   ]);
 });
 
