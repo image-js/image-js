b64f0c46e233b7c20fcdaea978ab72538c34a3fd
EscapedGibbon <maximgolubev2@gmail.com>
2026-06-26 09:59:30 +0200

    test: add test that doesn't work with current implementation

--- src/maskAnalysis/utils/__tests__/getConvexityDefects.test.ts
+++ src/maskAnalysis/utils/__tests__/getConvexityDefects.test.ts
@@ -44,3 +44,31 @@ test('carton mask', () => {
     { column: 116, row: 475 },
   ]);
 });
+
+// Current convexity defects implementation doesn't really support contours as
+// masks.
+test.todo('contour test', () => {
+  const mask = testUtils.createMask([
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+  ]);
+
+  const borderPoints = mask.getExternalContour();
+  const convexHull = mask.getConvexHull();
+  const defects = getConvexityDefects(borderPoints, convexHull.points, {
+    depthThreshold: 1,
+  });
+
+  expect(defects).toHaveLength(4);
+  expect(defects).toStrictEqual([
+    { column: 2, row: 3 },
+    { column: 3, row: 2 },
+    { column: 4, row: 3 },
+    { column: 3, row: 4 },
+  ]);
+});
