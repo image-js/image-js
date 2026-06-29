a609fd6d79c1337e8d14c74424996fd61b00fb99
greymoth-jp <m.hirakawa07@icloud.com>
2026-06-29 17:06:01 +0900

    fix: add resolution to BMP format

--- src/load/decodeBmp.ts
+++ src/load/decodeBmp.ts
@@ -4,6 +4,10 @@ import { Image } from '../Image.ts';
 import { Mask } from '../Mask.ts';
 import type { ImageColorModel } from '../utils/constants/colorModels.ts';
 
+import type { Resolution } from './load.types.ts';
+
+type DecodedBmp = ReturnType<typeof decode>;
+
 /**
  * Decode a BMP. See the fast-bmp npm module.
  * @param data - The data to decode.
@@ -32,6 +36,24 @@ export function decodeBmp(data: Uint8Array): Image {
     return new Image(decodedData.width, decodedData.height, {
       colorModel,
       data: decodedData.data as Uint8Array,
+      resolution: getBmpResolution(decodedData),
     });
   }
+}
+
+/**
+ * Gets image's resolution from its parsed data. BMP stores resolution in
+ * pixels per meter.
+ * @param bmp - Parsed BMP image.
+ * @returns Object with resolution data if it is defined.
+ */
+function getBmpResolution(bmp: DecodedBmp): Resolution | undefined {
+  if (!bmp.xPixelsPerMeter || !bmp.yPixelsPerMeter) {
+    return undefined;
+  }
+  return {
+    x: bmp.xPixelsPerMeter,
+    y: bmp.yPixelsPerMeter,
+    unit: 'meter',
+  };
 }
